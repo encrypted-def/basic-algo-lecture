84fea26f8a8ed373a2ddad0af0ba3d04030f49a3
blisstoner <blisstoner@gmail.com>
2020-03-11 12:03:33 +0900

    Create 10845_1.cpp

---
+++ 0x06/10845_1.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  queue<int> Q;
+  int n;
+  cin >> n;
+  while(n--){
+    string q;
+    cin >> q;
+    if(q=="push"){
+      int val;
+      cin >> val;
+      Q.push(val);
+    }
+    else if(q=="pop"){
+      if(Q.empty()) cout << -1 << '\n';
+      else{
+        cout << Q.front() << '\n';
+        Q.pop();
+      }
+    }
+    else if(q=="size"){
+      cout << Q.size() << '\n';
+    }
+    else if(q=="empty"){
+      cout << Q.empty() << '\n';
+    }    
+    else if(q=="front"){
+      if(Q.empty()) cout << -1 << '\n';
+      else cout << Q.front() << '\n';
+    }
+    else{ // back
+      if(Q.empty()) cout << -1 << '\n';
+      else cout << Q.back() << '\n';
+    }
+  }
+}

402efd74cf892ac9e97643088d77e3a5d749ff6a
blisstoner <blisstoner@gmail.com>
2020-03-13 14:44:32 +0900

    Update deque_test_ans.cpp

--- 0x07/deque_test_ans.cpp
+++ 0x07/deque_test_ans.cpp
@@ -5,12 +5,12 @@ const int MX = 1000005;
 int dat[2*MX+1];
 int head = MX, tail = MX;
 
-void push_front(int val){
-  dat[--head] = val;
+void push_front(int x){
+  dat[--head] = x;
 }
 
-void push_back(int val){
-  dat[tail++] = val;
+void push_back(int x){
+  dat[tail++] = x;
 }
 
 void pop_front(){

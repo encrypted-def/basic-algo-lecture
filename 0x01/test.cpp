f1f9edac3152de0a475c1d2bc8caeb5f2db815e4
blisstoner <blisstoner@gmail.com>
2020-03-16 12:31:52 +0900

    Create test.cpp

---
+++ 0x01/test.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func1(int N){
+  return -1;
+}
+
+int func2(int arr[], int N){
+  return -1;
+}
+
+int func3(int N){
+  return -1;
+}
+
+int func4(int N){
+  return -1;
+}
+
+void test1(){
+  int n[3] = {16, 34567, 27639};
+  int ans[3] = {60, 278812814, 178254968};
+  for(int i = 0; i < 3; i++){
+    int result = func1(n[i]);
+    cout << "TC #" << i << '\n';
+    cout << "expected : " << ans[i] << " result : " << result;
+    if(ans[i] == result) cout << " ... Correct!\n";
+    else cout << " ... Wrong!\n";
+  }
+}
+
+void test2(){
+  int arr[3][4] = {{1,52,48}, {50,42}, {4,13,63,87}};
+  int n[3] = {16, 34567, 27639};
+  int ans[3] = {1, 0, 1};
+  for(int i = 0; i < 3; i++){
+    int result = func2(arr[i], n[i]);
+    cout << "TC #" << i << '\n';
+    cout << "expected : " << ans[i] << " result : " << result;
+    if(ans[i] == result) cout << " ... Correct!\n";
+    else cout << " ... Wrong!\n";
+  }
+}
+
+void test3(){
+  int n[3] = {9, 693953651, 756580036};
+  int ans[3] = {1, 0, 1};
+  for(int i = 0; i < 3; i++){
+    int result = func3(n[i]);
+    cout << "TC #" << i << '\n';
+    cout << "expected : " << ans[i] << " result : " << result;
+    if(ans[i] == result) cout << " ... Correct!\n";
+    else cout << " ... Wrong!\n";
+  }
+}
+
+void test4(){
+  int n[3] = {5, 97615282, 1024};
+  int ans[3] = {4, 67108864, 1024};
+  for(int i = 0; i < 3; i++){
+    int result = func1(n[i]);
+    cout << "TC #" << i << '\n';
+    cout << "expected : " << ans[i] << " result : " << result;
+    if(ans[i] == result) cout << " ... Correct!\n";
+    else cout << " ... Wrong!\n";
+  }
+}
+
+int main(void){
+  test1();
+  test2();
+  test3();
+  test4();
+}

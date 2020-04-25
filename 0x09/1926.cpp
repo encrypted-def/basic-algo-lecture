be32e48410379554e369452dc0bb383915d984aa
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2020-04-25 22:36:07 +0900

    Update 1926.cpp

--- 0x09/1926.cpp
+++ 0x09/1926.cpp
@@ -32,8 +32,8 @@ int main(void){
         for(int dir = 0; dir < 4; dir++){ // 상하좌우 칸을 살펴볼 것이다.
           int nx = cur.X + dx[dir];
           int ny = cur.Y + dy[dir]; // nx, ny에 dir에서 정한 방향의 인접한 칸의 좌표가 들어감
-          if(nx < 0 or nx >= n or ny < 0 or ny >= m) continue; // 범위 밖일 경우 넘어감
-          if(vis[nx][ny] or board[nx][ny] != 1) continue; // 이미 방문한 칸이거나 파란 칸이 아닐 경우
+          if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue; // 범위 밖일 경우 넘어감
+          if(vis[nx][ny] || board[nx][ny] != 1) continue; // 이미 방문한 칸이거나 파란 칸이 아닐 경우
           vis[nx][ny] = 1; // (nx, ny)를 방문했다고 명시
           Q.push({nx,ny});
         }

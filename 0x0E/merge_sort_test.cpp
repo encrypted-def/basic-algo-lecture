20f0c6c673862bf39cd013599fabbf3ee7887c87
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2020-08-31 10:28:08 +0900

    Update merge_sort_test.cpp

--- 0x0E/merge_sort_test.cpp
+++ 0x0E/merge_sort_test.cpp
@@ -11,18 +11,18 @@ void merge(int st, int en){
   
 }
 
-// a[st:en]을 정렬하고 싶다.
+// arr[st:en]을 정렬하고 싶다.
 void merge_sort(int st, int en){
   if(en-st == 1) return; // 길이 1인 경우
   int mid = (st+en)/2;
-  merge_sort(st,mid); // arr[st:mid]을 정렬한다.
-  merge_sort(mid,en); // arr[mid:en]을 정렬한다.
-  merge(st,en); // arr[st:mid]와 arr[mid:en]을 합친다.
+  merge_sort(st, mid); // arr[st:mid]을 정렬한다.
+  merge_sort(mid, en); // arr[mid:en]을 정렬한다.
+  merge(st, en); // arr[st:mid]와 arr[mid:en]을 합친다.
 }
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  merge_sort(0,n);
-  for(int i = 0; i < n; i++) cout << arr[i] << ' ';
+  merge_sort(0, n);
+  for(int i = 0; i < n; i++) cout << arr[i] << ' ';  // -53 3 12 15 16 22 23 25 46 357
 }

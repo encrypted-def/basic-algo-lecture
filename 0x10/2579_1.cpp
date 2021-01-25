92d191fb9fa1cac19710f88c04bbb1b20137baf2
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-01-25 16:54:04 +0900

    Update 2579_1.cpp

--- 0x10/2579_1.cpp
+++ 0x10/2579_1.cpp
@@ -1,27 +1,27 @@
-// http://boj.kr/ffe255761ed34ed4972855b85cb958af
+// http://boj.kr/2b21f929cae94416a00de3ed8c5417ff
 #include <bits/stdc++.h>
 using namespace std;
 
 int s[305];
 int n;
-int d[305][2];
+int d[305][3];
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   cin >> n;
-  for(int i = 0; i < n; i++) cin >> s[i];
+  for(int i = 1; i <= n; i++) cin >> s[i];
   if(n==1){
-    cout << s[0];
+    cout << s[1];
     return 0;
   }
-  d[0][0] = s[0];
-  d[0][1] = 0;
-  d[1][0] = s[1];
-  d[1][1] = s[0]+s[1];
-  for(int i = 2; i < n; i++){
-    d[i][0] = max(d[i-2][0],d[i-2][1])+s[i];
-    d[i][1] = d[i-1][0]+s[i];
+  d[1][1] = s[1];
+  d[1][2] = 0;
+  d[2][1] = s[2];
+  d[2][2] = s[1]+s[2];
+  for(int i = 3; i <= n; i++){
+    d[i][1] = max(d[i-2][1],d[i-2][2])+s[i];
+    d[i][2] = d[i-1][1]+s[i];
   }
-  cout << max(d[n-1][0],d[n-1][1]);
+  cout << max(d[n][1],d[n][2]);
 }

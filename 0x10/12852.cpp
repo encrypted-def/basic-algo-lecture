75cbbed3f3c9a8813b80344737eaae30e3cc281d
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-01-28 03:33:09 +0900

    Create 12852.cpp

---
+++ 0x10/12852.cpp
@@ -0,0 +1,33 @@
+// http://boj.kr/da0497aabb26436c9ae613785cb439f7
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[1000005];
+int pre[1000005];
+int n;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  d[1] = 0;
+  for(int i = 2; i <= n; i++){
+    d[i] = d[i-1] + 1;
+    pre[i] = i-1;
+    if(i%2 == 0 && d[i] > d[i/2] + 1){
+      d[i] = d[i/2] + 1;
+      pre[i] = i/2;
+    }
+    if(i%3 == 0 && d[i] > d[i/3] + 1){
+      d[i] = d[i/3] + 1;
+      pre[i] = i/3;
+    }
+  }
+  cout << d[n] << '\n';
+  int cur = n;
+  while(true){
+    cout << cur << ' ';
+    if(cur == 1) break;
+    cur = pre[cur];    
+  }
+}

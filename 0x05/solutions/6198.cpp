277ccf5fc537b65fe6517d4a1d210fb3216cefe8
yoonsung.jung <ybook2012@gmail.com>
2021-04-10 18:41:21 +0900

    Update : 6198.cpp

--- 0x05/solutions/6198.cpp
+++ 0x05/solutions/6198.cpp
@@ -1,11 +1,48 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : unluckyjung
 // Co-authored by : -
-// http://boj.kr/****************
-#include <bits/stdc++.h>
+// http://boj.kr/a84f083cdee3436f9f46acdef175e55f
+
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-int main(void){
+stack<int> s;
+int n;
+ll answer;
+
+void view_check(ll building) {
+	while (!s.empty()) {
+		if (building >= s.top()) s.pop();
+		else {
+			answer += s.size();
+			return;
+		}
+	}
+}
+
+void solve() {
+	ll building;
+	while (n--) {
+		cin >> building;
+
+		if (!s.empty()){
+			if (building < s.top()) answer += s.size();
+			else view_check(building);
+		}
+		s.push(building);
+	}
+	cout << answer;
+}
+
+void input() {
+	cin >> n;
+}
+
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+	input();
+	solve();
+	return 0;
 }

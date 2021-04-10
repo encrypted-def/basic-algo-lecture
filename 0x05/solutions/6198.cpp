5fedbfb1f65bfbd886b995a0b669d24bcdab2be0
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-04-10 20:21:05 +0900

    Update 6198.cpp

--- 0x05/solutions/6198.cpp
+++ 0x05/solutions/6198.cpp
@@ -1,48 +1,29 @@
 // Authored by : unluckyjung
-// Co-authored by : -
+// Co-authored by : BaaaaaaaaaaarkingDog
 // http://boj.kr/a84f083cdee3436f9f46acdef175e55f
 
-#include<bits/stdc++.h>
-#define ll long long
+#include <bits/stdc++.h>
 using namespace std;
+
+#define ll long long
 
 stack<int> s;
 int n;
-ll answer;
-
-void view_check(ll building) {
-	while (!s.empty()) {
-		if (building >= s.top()) s.pop();
-		else {
-			answer += s.size();
-			return;
-		}
-	}
-}
-
-void solve() {
-	ll building;
-	while (n--) {
-		cin >> building;
-
-		if (!s.empty()){
-			if (building < s.top()) answer += s.size();
-			else view_check(building);
-		}
-		s.push(building);
-	}
-	cout << answer;
-}
-
-void input() {
-	cin >> n;
-}
+ll ans;
 
 int main() {
-  ios::sync_with_stdio(0);
+  ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-	input();
-	solve();
-	return 0;
+  cin >> n;
+  ll h;
+  while (n--) {
+    cin >> h;
+    while(!s.empty() && s.top() <= h)
+      s.pop();
+    ans += s.size();
+    s.push(h);
+  }
+  cout << ans;
+  return 0;
 }

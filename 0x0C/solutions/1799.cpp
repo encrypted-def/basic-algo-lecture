0944f9180657dce891b63fc97614ed4f84903401
hehehwangPC <hehehwang@gmail.com>
2021-04-12 02:18:28 +0900

    Update 1799.cpp

--- 0x0C/solutions/1799.cpp
+++ 0x0C/solutions/1799.cpp
@@ -1,11 +1,42 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : heheHwang
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/d3cbc0df3bc34ea0899616c42cb8f1e3
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+vector<pair<int, int>> board[2][20];
+bool used[2][20];
+int ans[2];
+int N;
+void bishop(int idx, int cnt, int color) {
+  if (idx == 2 * N) {
+    ans[color] = max(ans[color], cnt);
+    return;
+  }
+  for (auto v : board[color][idx]) {
+    int x, y;
+    tie(x, y) = v;
+    if (used[color][x + y]) continue;
+    used[color][x + y] = true;
+    bishop(idx + 1, cnt + 1, color);
+    used[color][x + y] = false;
+  }
+  bishop(idx + 1, cnt, color);
+}
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> N;
+  int d;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cin >> d;
+      if (d)
+        board[(i + j + 1) % 2][N + i - j - 1].push_back({i, j});
+    }
+  }
+  bishop(0, 0, 0);
+  bishop(0, 0, 1);
+  cout << ans[0] + ans[1];
 }

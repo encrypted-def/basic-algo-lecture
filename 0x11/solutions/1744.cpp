754fc961505eb71e42d4032e34adfc45b107978f
heheHwang <hehehwang@gmail.com>
2021-04-18 10:28:41 +0900

    Update 1744.cpp

--- 0x11/solutions/1744.cpp
+++ 0x11/solutions/1744.cpp
@@ -1,11 +1,37 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : heheHwang
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/97184a63b6e84d88b7da20a0a435c0e5
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+long long ans;
+void seqSum(vector<int> v) {
+  while (1 < v.size()) {
+    ans += *(v.end() - 1) * *(v.end() - 2);
+    v.pop_back();
+    v.pop_back();
+  }
+  if (v.size())
+    ans += v[0];
+}
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  vector<int> seqP, seqN;
+  int N, t;
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    cin >> t;
+    if (t == 1) ans++;
+    else if (0 < t)
+      seqP.push_back(t);
+    else
+      seqN.push_back(t);
+  }
+  sort(seqP.begin(), seqP.end());
+  sort(seqN.begin(), seqN.end(), greater<>());
+  seqSum(seqP);
+  seqSum(seqN);
+  cout << ans;
 }

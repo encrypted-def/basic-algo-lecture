2a97da77f54bbd227d3c475c5a872eda09ac509c
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-04-21 23:02:03 +0900

    Update 10844.cpp

--- 0x10/solutions/10844.cpp
+++ 0x10/solutions/10844.cpp
@@ -1,20 +1,20 @@
 // Authored by : tongnamuu
-// Co-authored by : -
-// http://boj.kr/8f60a6d53702418398fd7eb76bc5442c
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/03ae0f4b94fb463f9b0426fad0a82244
 #include <bits/stdc++.h>
 using namespace std;
 long long d[101][10];
-int main(void) {
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   int n;
   cin >> n;
   for (int i = 1; i <= 9; ++i) d[1][i] = 1;
-
+  
   for (int i = 2; i <= n; ++i) {
     for (int k = 0; k <= 9; ++k) {
-      if (k - 1 >= 0) d[i][k] += d[i - 1][k - 1];
-      if (k + 1 <= 9) d[i][k] += d[i - 1][k + 1];
+      if (k != 0) d[i][k] += d[i - 1][k - 1];
+      if (k != 9) d[i][k] += d[i - 1][k + 1];
       d[i][k] %= 1000000000;
     }
   }

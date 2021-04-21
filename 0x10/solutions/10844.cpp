1edc53ee08a1fac6bb13a9876e05befe6f50e51b
tongnamuu <thban33@naver.com>
2021-04-21 17:19:00 +0900

    boj 10844

--- 0x10/solutions/10844.cpp
+++ 0x10/solutions/10844.cpp
@@ -1,11 +1,26 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : tongnamuu
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/3019d8917d694328a4f314b7d8228dec
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
+long long d[101][10];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+  int n;
+  cin >> n;
+  for (int i = 1; i <= 9; ++i) d[1][i] = 1;
+  for (int i = 2; i <= n; ++i) {
+    for (int k = 0; k <= 9; ++k) {
+      if (k - 1 >= 0) d[i][k] += d[i - 1][k - 1];
+      if (k + 1 <= 9) d[i][k] += d[i - 1][k + 1];
+      d[i][k] %= 1000000000;
+    }
+  }
+  long long ans = 0;
+  for (int i = 0; i <= 9; ++i) {
+    ans += d[n][i];
+  }
+  ans %= 1000000000;
+  cout << ans;
 }

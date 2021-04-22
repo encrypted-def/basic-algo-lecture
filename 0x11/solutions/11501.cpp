297666ffaf5543c9b68dd180779e04294a2c5a85
tongnamuu <thban33@naver.com>
2021-04-22 16:33:54 +0900

    Revert "boj 11501"
    This reverts commit 9b5343dce6e795b5ef338584c3c5923003de2b99.

--- 0x11/solutions/11501.cpp
+++ 0x11/solutions/11501.cpp
@@ -1,24 +1,11 @@
-// Authored by : tongnamuu
+// Authored by : BaaaaaaaaaaarkingDog
 // Co-authored by : -
-// http://boj.kr/4217173611354888b0ebcc96b3cf522a
+// http://boj.kr/****************
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[1000001];
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int T;cin>>T;
-  while(T--){
-    int n;
-    cin>>n;
-    for(int i=0;i<n;++i) cin>>a[i];
-    int max_val = a[n-1];
-    long long ans = 0;
-    for(int i=n-2;i>=0;--i) {
-      if(a[i] > max_val) max_val = a[i];
-      ans += max_val - a[i];
-    }
-    cout<<ans<<'\n';
-  }
+  
 }

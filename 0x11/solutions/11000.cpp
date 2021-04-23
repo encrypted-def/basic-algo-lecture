7dc2f7d1b4740d79e08cc0b273009c4262c2513d
heheHwang <hehehwang@gmail.com>
2021-04-23 23:30:43 +0900

    Update 11000.cpp

--- 0x11/solutions/11000.cpp
+++ 0x11/solutions/11000.cpp
@@ -1,11 +1,30 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : heheHwang
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/422c1bde5a5b422082829cd06a7565a1
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int N;
+pair<int, int> lecture[200'002];
+priority_queue<int, vector<int>, greater<>> pQ;
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> N;
+  int a, b;
+  for (int i = 0; i < N; i++) {
+    cin >> a >> b;
+    lecture[i] = {a, b};
+  }
+  pQ.push(0);
+  sort(lecture, lecture + N);
+  for (int i = 0; i < N; i++) {
+    int s, e, c = pQ.top();
+    tie(s, e) = lecture[i];
+    pQ.pop();
+    if (s < c) pQ.push(c);
+    pQ.push(e);
+  }
+  cout << pQ.size();
 }

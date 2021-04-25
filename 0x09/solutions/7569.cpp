3fbd6f58bcbb3470a9fd5848e77606f1453c0929
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-04-25 09:34:44 +0900

    Update 7569.cpp

--- 0x09/solutions/7569.cpp
+++ 0x09/solutions/7569.cpp
@@ -1,41 +1,37 @@
 // Authored by : BueVonHun
-// Co-authored by : -
-// http://boj.kr/2f20289998684fcda4d28da3fd3a8a26
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/f3877bee8d3c454482dd3229e4246b49
 #include <bits/stdc++.h>
-#include "iostream"
-#include "queue"
-#include "tuple"
-
 using namespace std;
 
 int dx[6] = {0, 0, 1, -1, 0, 0};
 int dy[6] = {1, -1, 0, 0, 0, 0};
 int dz[6] = {0, 0, 0, 0, 1, -1};
-int board[1003][1003][1003];
-int dist[1003][1003][1003];
+int board[103][103][103];
+int dist[103][103][103];
 queue<tuple<int, int, int>> Q;
 int m, n, h;
 int main(void) {
-  ios_base::sync_with_stdio(false);
+  ios_base::sync_with_stdio(0);
   cin.tie(0);
   cin >> m >> n >> h;
   for (int i = 0; i < h; i++) {
     for (int j = 0; j < n; j++) {
-	  for (int k = 0; k < m; k++) {
-	    int tmp;
-	    cin >> tmp;
-		board[j][k][i] = tmp;
-		if (tmp==1) Q.push({j, k, i});
-        if (tmp==0) dist[j][k][i] = -1;
-	  }
+      for (int k = 0; k < m; k++) {
+        int tmp;
+        cin >> tmp;
+        board[j][k][i] = tmp;
+        if (tmp == 1) Q.push({j, k, i});
+        if (tmp == 0) dist[j][k][i] = -1;
+      }
     }
   }
 
   while (!Q.empty()) {
-    auto cur = Q.front(); Q.pop();
-	int curX = get<0>(cur);
-    int curY = get<1>(cur);
-    int curZ = get<2>(cur);
+    auto cur = Q.front();
+    Q.pop();
+    int curX, curY, curZ;
+    tie(curX, curY, curZ) = cur;
     for (int dir = 0; dir < 6; dir++) {
       int nx = curX + dx[dir];
       int ny = curY + dy[dir];
@@ -51,7 +47,7 @@ int main(void) {
   for (int i = 0; i < h; i++) {
     for (int j = 0; j < n; j++) {
       for (int k = 0; k < m; k++) {
-        if (dist[j][k][i]==-1) {
+        if (dist[j][k][i] == -1) {
           cout << -1 << "\n";
           return 0;
         }

93a0a76281537a4baefe4ae68269f6e255845253
tongnamuu <thban33@naver.com>
2021-04-27 21:17:16 +0900

    remove unnecessary array

--- 0x10/solutions/11052.cpp
+++ 0x10/solutions/11052.cpp
@@ -1,19 +1,15 @@
 // Authored by : tongnamuu
 // Co-authored by : -
-// http://boj.kr/42d1214f12244dad978c1b1ec82679f2
+// http://boj.kr/e8bb71d963ad4c0e87544b09d676ad8e
 #include <bits/stdc++.h>
 using namespace std;
-
-int a[1001];
 int d[1001];
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   int n;cin>>n;
-  for(int i=1;i<=n;++i){
-    cin>>a[i];
-    d[i] = a[i];
-  }
+  for(int i=1;i<=n;++i) cin>>d[i];
+  
   for(int i=1;i<=n;++i) {
     for(int j=1;j<i;++j) {
       d[i] = max(d[i-j] + d[j], d[i]);

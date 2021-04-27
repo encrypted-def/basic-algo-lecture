96f8e9c3bc7edbb5b76366f9446955fa79c13437
tongnamuu <thban33@naver.com>
2021-04-27 21:19:16 +0900

    fix code convention

--- 0x10/solutions/11052.cpp
+++ 0x10/solutions/11052.cpp
@@ -1,6 +1,6 @@
 // Authored by : tongnamuu
 // Co-authored by : -
-// http://boj.kr/e8bb71d963ad4c0e87544b09d676ad8e
+// http://boj.kr/a9eee96f33c54876b9d5ba0b1c3bb246
 #include <bits/stdc++.h>
 using namespace std;
 int d[1001];
@@ -9,11 +9,6 @@ int main(void){
   cin.tie(0);
   int n;cin>>n;
   for(int i=1;i<=n;++i) cin>>d[i];
-  
-  for(int i=1;i<=n;++i) {
-    for(int j=1;j<i;++j) {
-      d[i] = max(d[i-j] + d[j], d[i]);
-    }
-  }
+  for(int i=1;i<=n;++i) for(int j=1;j<i;++j) d[i] = max(d[i-j] + d[j], d[i]);
   cout << d[n];
 }

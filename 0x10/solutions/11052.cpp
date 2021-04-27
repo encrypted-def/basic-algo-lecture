a8fd90ffa855ae1223bb494f132dfee5dc94d8e3
tongnamuu <thban33@naver.com>
2021-04-27 21:14:09 +0900

    boj 11052

--- 0x10/solutions/11052.cpp
+++ 0x10/solutions/11052.cpp
@@ -1,11 +1,23 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : tongnamuu
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/42d1214f12244dad978c1b1ec82679f2
 #include <bits/stdc++.h>
 using namespace std;
 
+int a[1001];
+int d[1001];
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  int n;cin>>n;
+  for(int i=1;i<=n;++i){
+    cin>>a[i];
+    d[i] = a[i];
+  }
+  for(int i=1;i<=n;++i) {
+    for(int j=1;j<i;++j) {
+      d[i] = max(d[i-j] + d[j], d[i]);
+    }
+  }
+  cout << d[n];
 }

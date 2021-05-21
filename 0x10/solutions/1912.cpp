a3f404beff42fb61b8f6063a2a104bddb408851a
Hot6Maina <65355710+Hot6Maina@users.noreply.github.com>
2021-05-21 14:47:40 +0900

    Update 1912.cpp

--- 0x10/solutions/1912.cpp
+++ 0x10/solutions/1912.cpp
@@ -1,11 +1,22 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : Hot6Mania
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/a9a359faddb34f0faf591d3421b0e6af
 #include <bits/stdc++.h>
 using namespace std;
+
+int n;
+int a[100010], d[100010];
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   
+  cin >> n;
+  for(int i = 1; i <= n; ++i){
+    cin >> a[i];
+    d[i] = a[i];
+  }
+  for(int i = 1; i <= n; ++i)
+    d[i] = max(d[i], d[i-1] + a[i]);
+  cout << *max_element(d + 1, d + n + 1);
 }

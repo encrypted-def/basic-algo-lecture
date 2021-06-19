1b90057569e5cdd68cc656adcc93d82aa4dcd06f
twinkite <yeonni_9657@naver.com>
2021-06-19 15:08:54 +0900

    Update 2752.cpp

--- 0x02/solutions/2752.cpp
+++ 0x02/solutions/2752.cpp
@@ -1,11 +1,21 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : twinkite
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/e1c98eb9700742e2bacb8db5545002de
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  int arr[4];
+  for(int i=0;i<3;i++){
+      cin>>arr[i];
+  }
+  sort(arr, arr+3);
+  for(int i=0;i<3;i++){
+      cout<<arr[i]<<" ";
+  }
+
+  return 0;
 }

03ef87e6f26923b362237da9f6e38aa71720bd54
twinkite <yeonni_9657@naver.com>
2021-06-19 15:14:12 +0900

    Update 10171.cpp

--- 0x02/solutions/10171.cpp
+++ 0x02/solutions/10171.cpp
@@ -1,11 +1,14 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : twinkite
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/b9842be631924421b154ec78c43d88b0
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cout<<"\\    /\\"<<'\n'<<" )  ( \')"<<'\n'<<"(  /  )"<<'\n'<<" \\(__)|";
+
+  return 0;
 }

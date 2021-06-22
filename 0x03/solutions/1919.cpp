d94a1d58e0d726f655c31621d2dd3881f69a3611
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-06-22 17:42:57 +0900

    Update 1919.cpp

--- 0x03/solutions/1919.cpp
+++ 0x03/solutions/1919.cpp
@@ -1,28 +1,25 @@
-// Authored by : twinkite
-// Co-authored by : -
-// http://boj.kr/113a0c8415164751b8316d0dcc589658
+// Authored by : BaaaaaaaaaaarkingDog
+// Co-authored by : twinkite
+// http://boj.kr/ae5d8d2f69f04530b4df0c591e9b07d5
 #include <bits/stdc++.h>
 using namespace std;
+
+int arr[2][26], res;
+string s1, s2;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  int res = 0;
-  string s1, s2;
-  int arr[2][26];
-  fill(arr[0], arr[0]+26, 0);
-  fill(arr[1], arr[1]+26, 0);
+ 
   cin>>s1>>s2;
-  for(int i=0;i<s1.size();i++){
-      arr[0][s1[i]-'a']++;
-  }
-  for(int i=0;i<s2.size();i++){
-      arr[1][s2[i]-'a']++;
-  }
-  for(int i=0;i<26;i++){
-      res+=abs(arr[0][i]-arr[1][i]);
-  }
-  cout<<res;
-  return 0;
+  for(char c : s1)
+    arr[0][c-'a']++;
+  
+  for(char c : s2)
+    arr[1][c-'a']++;
+  
+  for(int i=0; i<26; i++)
+    res += abs(arr[0][i]-arr[1][i]);
+  
+  cout << res;
 }

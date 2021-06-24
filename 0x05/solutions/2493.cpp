c720a53e43e3a313881ffc57043c59c647e36433
twinkite <yeonni_9657@naver.com>
2021-06-24 17:13:35 +0900

    Update 2493.cpp

--- 0x05/solutions/2493.cpp
+++ 0x05/solutions/2493.cpp
@@ -1,11 +1,32 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : twinkite
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/a8dabc136c904aebaef9a41139d945f5
 #include <bits/stdc++.h>
 using namespace std;
+#define pii pair<int, int>
+#define x first
+#define y second
 
-int main(void){
-  ios::sync_with_stdio(0);
+int main(){
+  ios_base::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  stack<pii> tower;
+  int N, temp; 
+  cin>>N;
+  tower.push({100000001,0});
+  for(int i=1;i<=N;i++){
+      cin>>temp;
+      if(tower.top().x>temp){
+          cout<<tower.top().y<<" ";
+          tower.push({temp,i});
+      } else {
+          while(tower.top().x<temp){
+              tower.pop();
+          }
+          cout<<tower.top().y<<" ";
+          tower.push({temp,i});
+      }
+  }
+  return 0;
 }

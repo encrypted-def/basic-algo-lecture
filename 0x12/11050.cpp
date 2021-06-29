0f4545afbe0e379bf5f1e21bfe0e7ed9e39db389
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-06-29 10:24:29 +0900

    Create 11050.cpp

---
+++ 0x12/11050.cpp
@@ -0,0 +1,15 @@
+// http://boj.kr/88189dab63974f8e9446dd9d960aea56
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n,k;
+  cin >> n >> k;
+  int ret = 1;
+  for(int i = 1; i <= n; i++) ret *= i;
+  for(int i = 1; i <= k; i++) ret /= i;
+  for(int i = 1; i <= n-k; i++) ret /= i;
+  cout << ret;
+}

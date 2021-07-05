8b493e655b2223a454ec8beee28d7401f30a6dd9
pha-ran <pha-ran@naver.com>
2021-07-05 21:55:52 +0900

    Update 10093.cpp

--- 0x02/solutions/10093.cpp
+++ 0x02/solutions/10093.cpp
@@ -1,6 +1,6 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : pha-ran (soft18)
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/b8dd85f62e494e5585190f114bb59e45
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,4 +8,12 @@ int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   
+  long long a, b;
+  cin >> a >> b;
+  if (a > b) swap(a, b);
+  if (a == b || b - a == 1) cout << 0;
+  else {
+    cout << b - a - 1 << "\n";
+    while (++a < b) cout << a << " ";
+  }
 }

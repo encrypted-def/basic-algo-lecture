c738840c7441a99b8bab02f283b80d2d671ccec8
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-07-14 23:17:38 +0900

    Update 1963.cpp

--- 0x12/solutions/1963.cpp
+++ 0x12/solutions/1963.cpp
@@ -1,77 +1,61 @@
 // Authored by : heheHwang
-// Co-authored by : -
-// http://boj.kr/2385b7f2a4da406f921ee64603eba658
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/db8b8c85ca584c5fb3250b395bc7e678
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<bool> seive(10002, true);
-vector<int> primes;
-vector<int> adj[10002];
-int pwr(int n) {
-  int r = 1;
-  for (int i = 0; i < n; i++) r *= 10;
-  return r;
-}
+vector<bool> sieve(10002, true);
+
 int vec2num(vector<int> &vec) {
   int r = 0;
-  for (int i = 0; i < 4; i++) r += vec[i] * pwr(i);
+  int p10 = 1;
+  for (int i = 0; i < 4; i++){
+    r += vec[i] * p10;
+    p10 *= 10;
+  }
   return r;
 }
+
 vector<int> num2vec(int num){
   vector<int> rtn;
   do rtn.push_back(num % 10);
   while (num /= 10);
   return rtn;
 }
+
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  seive[1] = false;
+  sieve[1] = false;
   for (int i = 2; i * i < 10000; i++)
-    for (int j = i * i; j < 10000; j += i) seive[j] = false;
-  for (int i = 1000; i < 10000; i++)
-    if (seive[i]) primes.push_back(i);
+    for (int j = i * i; j < 10000; j += i) sieve[j] = false;
 
-  for (int p : primes) { // p == 1279
-    vector<int> num = num2vec(p); // num == {9, 7, 2, 1}
-    for (int i = 0; i < 4; i++) {
-      int t = num[i];
-      for (int j = num[i] + 1; j < 10; j++) {
-        num[i] = j;
-        int changed = vec2num(num);
-        if (seive[changed]) {
-          adj[p].push_back(changed);
-          adj[changed].push_back(p);
-        }
-      }
-      num[i] = t;
-    }
-  }
   int tc, s, e;
   cin >> tc;
   for (int i = 0; i < tc; i++) {
     cin >> s >> e;
-    queue<pair<int, int>> Q = {};
-    vector<bool> vis(10002, 0);
-    Q.push({s, 0});
-    while (1) {
-      if (Q.empty()) {
-        cout << "Impossible" << '\n';
-        break;
-      }
-      int num, cnt;
-      tie(num, cnt) = Q.front();
+    queue<int> Q;
+    Q.push(s);
+    int dist[10004];
+    fill(dist,dist+10001,-1);
+    dist[s] = 0;
+    while (!Q.empty()) {
+      int cur = Q.front();
       Q.pop();
-      if (num == e) {
-        cout << cnt << '\n';
-        break;
-      }
-      for (int v : adj[num]) {
-        if (vis[v]) continue;
-        Q.push({v, cnt + 1});
-        vis[v] = true;
+      for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 10; j++){
+          if(i == 3 && j == 0) continue;
+          vector<int> nxt_digits = num2vec(cur);
+          nxt_digits[i] = j;
+          int nxt = vec2num(nxt_digits);
+          if(!sieve[nxt] || dist[nxt] != -1) continue;
+          dist[nxt] = dist[cur] + 1;
+          Q.push(nxt);
+        }
       }
     }
-  }
+    if(dist[e] == -1) cout << "Impossible\n";
+    else cout << dist[e] << '\n';
+  }  
 }

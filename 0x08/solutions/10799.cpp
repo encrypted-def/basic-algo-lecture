778d734ff174bd1248b46e14714130b2eebe0b69
Hun, 태훈 <46879264+Bue-von-hon@users.noreply.github.com>
2021-07-19 14:49:20 +0900

    Update 10799.cpp
    사실 그냥 lt, rt 변수 만들어서 "(", ")" 갯수를 카운팅하여 풀려고 하였으나, 블로그에 올라와있는 풀이 그대로를 구현하는게 맞다 생각되어 스택 풀이를 구현하여 PR드립니다
    레이저인지 아닌지도 블로그 풀이를 사용하여 판단하였고, 정답 갱신 역시 블로그 내용을 온전히 구현하였습니다

--- 0x08/solutions/10799.cpp
+++ 0x08/solutions/10799.cpp
@@ -1,11 +1,34 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : BueVonHun
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/926a4c383a464de3a85607a1d9db87a4
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-
+string s;
+ll ans = 0;
+stack<char> st;
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  int sz = s.length();
+  bool f = false;
+  for (int i = 0; i < sz; i++) {
+    if (s[i]=='(') {
+      f = true;
+      st.push(s[i]);
+    }
+    else if(s[i]==')') {
+      if (f) {
+        st.pop();
+        ans+=st.size();
+      }
+      else {
+        st.pop();
+        ans++;
+      }
+      f = false;
+    }
+  }
+  cout << ans << "\n";
+  return 0;
 }

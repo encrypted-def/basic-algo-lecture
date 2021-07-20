c913110c6dc160a587423600e68a48ee4ed29443
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-07-20 18:44:37 +0900

    Update 16920.cpp

--- 0x09/solutions/16920.cpp
+++ 0x09/solutions/16920.cpp
@@ -1,6 +1,6 @@
 // Authored by : qhsl1213
-// Co-authored by : -
-// http://boj.kr/7ec87f5e05d04ac785abcf49bed8bab9
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/2adb444701754f48baa8b965bb9a9b05
 #include <bits/stdc++.h>
 using namespace std;
 int canExtend[1000][1000];
@@ -8,16 +8,13 @@ int step[10];
 int area[10];
 int dx[4] = {0, 0, 1, -1};
 int dy[4] = {1, -1, 0, 0};
-queue<pair<pair<int, int>, int>> q[10];
+queue<tuple<int, int, int>> q[10];
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   int N, M, P;
   cin >> N >> M >> P;
-  // 큐 배열에 큐 생성
-  for(int i=1; i<=P; i++)
-    q[i] = queue<pair<pair<int ,int>, int>>();
   // 각 플레이어의 이동 제한 횟수 입력
   for(int i=1; i<=P; i++)
     cin >> step[i];
@@ -30,7 +27,7 @@ int main() {
       else if(ch == '#') canExtend[i][j] = 0;
       else{
         canExtend[i][j] = 0;
-        q[ch - '0'].push({{i, j}, 0}); // 각 플레이어의 초기 영토 기록
+        q[ch - '0'].push({i, j, 0}); // 각 플레이어의 초기 영토 기록
         area[ch - '0'] += 1;
       }
     }
@@ -40,20 +37,20 @@ int main() {
     bool isExtend = 0;
     // 1~P번 플레이어의 순차적인 영토 확장
     for (int i = 1; i <= P; i++) {
-      queue<pair<pair<int, int>, int>> nextq; // 다음 턴에 확장을 고려할 영토 기록
+      queue<tuple<int,int,int>> nextq; // 다음 턴에 확장을 고려할 영토 기록
       while (!q[i].empty()) {
-        int curx = q[i].front().first.first, cury = q[i].front().first.second;
-        int curstep = q[i].front().second;
+        int curx, cury, curstep;
+        tie(curx, cury, curstep) = q[i].front();
         q[i].pop();
         if (curstep >= step[i]) {
-          nextq.push({{curx, cury}, 0});
+          nextq.push({curx, cury, 0});
           continue;
         }
         for (int dir = 0; dir < 4; dir++) {
           int x = curx + dx[dir], y = cury + dy[dir];
           if (x < 0 || x >= N || y < 0 || y >= M) continue;
           if (!canExtend[x][y]) continue;
-          q[i].push({{x, y}, curstep+1});
+          q[i].push({x, y, curstep+1});
           canExtend[x][y] = 0;
           area[i]++;
           isExtend = 1;

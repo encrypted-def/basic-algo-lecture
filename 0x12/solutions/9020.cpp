860203581287280078835476f902007f8e208936
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-07-26 00:22:18 +0900

    Update 9020.cpp

--- 0x12/solutions/9020.cpp
+++ 0x12/solutions/9020.cpp
@@ -1,18 +1,18 @@
 // Authored by : heheHwang
-// Co-authored by : -
-// http://boj.kr/1bfaf1e6669d4f82ab09af60d6897020
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/45e26df635a140df9354fb7d3fbf0497
 #include <bits/stdc++.h>
 using namespace std;
 
 const int MXN = 10'002; 
-vector<bool> seive(MXN, 1);
+vector<bool> sieve(MXN, 1);
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   for (int i = 2; i*i < MXN ; i++){
-    if (!seive[i]) continue;
-    for (int j = i * i; j <= MXN; j += i) seive[j] = false;
+    if (!sieve[i]) continue;
+    for (int j = i * i; j <= MXN; j += i) sieve[j] = false;
   } 
 
   int tc;
@@ -22,7 +22,7 @@ int main(void) {
     cin >> c;
     for (int a = c/2; 0 < a; a--){
       int b = c - a;
-      if (seive[a] && seive[b]){
+      if (sieve[a] && sieve[b]){
         cout << a << ' ' << b << '\n';
         break;
       }

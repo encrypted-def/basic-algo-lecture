9b745168a384eb13dc7d6d67260b3739359289fb
jaeho <wogha95@naver.com>
2021-08-04 12:25:00 +0900

    Update 2439.cpp

--- 0x02/solutions/2439.cpp
+++ 0x02/solutions/2439.cpp
@@ -1,6 +1,6 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : wogha95
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/e506f4f9f3074de293fcde43c4a45208
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,4 +8,13 @@ int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   
+  int N;
+  cin >> N;
+
+  for(int i = 0; i < N; i++) {
+    int j = 0;
+    for( ; j < N - i - 1; j++)  cout << ' ';
+    for( ; j < N; j++)  cout << '*';
+    cout << "\n";
+  }
 }

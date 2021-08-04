956d14287f2c8a4818a9ea9d2e6b8d2255b9c73f
jaeho <wogha95@naver.com>
2021-08-04 22:58:48 +0900

    Update 2753.cpp

--- 0x02/solutions/2753.cpp
+++ 0x02/solutions/2753.cpp
@@ -1,11 +1,23 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : wogha95
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/87e6f4cf7583445fb3ecc75574792f92
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+    
+  int year;
+  cin >> year;
+    
+  if(year % 4 == 0)
+    // 4의 배수이면서 400의 배수
+    if(year % 400 == 0)  cout << 1;
+    // 4의 배수이면서 100의 배수
+    else if(year % 100 == 0)  cout << 0;
+    // 4의 배수이면서 100, 400의 배수가 아닌 수
+    else  cout << 1;
+  // 4의 배수가 아닌 수
+  else  cout << 0;
 }

944f3ec0d9356f9ba88f12fe0b3e75945a3aab06
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-08-04 19:18:55 +0900

    Update 2438.cpp

--- 0x02/solutions/2438.cpp
+++ 0x02/solutions/2438.cpp
@@ -1,6 +1,6 @@
-// Authored by : BaaaaaaaaaaarkingDog
-// Co-authored by : -
-// http://boj.kr/****************
+// Authored by : wogha95
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/b6adf7a5cfeb49f7a45563ac675a6b3a
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,4 +8,11 @@ int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   
+  int N;
+  cin >> N;
+
+  for(int i = 0; i < N; i++) {
+    for(int j = 0; j <= i; j++)  cout << '*';
+    cout << "\n";
+  }
 }

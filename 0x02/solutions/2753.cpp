f56b25946009d41b38acb316b0a4361138c30b05
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-08-05 13:08:02 +0900

    Update 2753.cpp

--- 0x02/solutions/2753.cpp
+++ 0x02/solutions/2753.cpp
@@ -1,6 +1,6 @@
 // Authored by : wogha95
-// Co-authored by : -
-// http://boj.kr/87e6f4cf7583445fb3ecc75574792f92
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/5306be11066640fb96fa672ccd15b40c
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,13 +11,14 @@ int main(void) {
   int year;
   cin >> year;
     
-  if(year % 4 == 0)
+  if(year % 4 == 0){
     // 4의 배수이면서 400의 배수
     if(year % 400 == 0)  cout << 1;
     // 4의 배수이면서 100의 배수
     else if(year % 100 == 0)  cout << 0;
     // 4의 배수이면서 100, 400의 배수가 아닌 수
-    else  cout << 1;
+    else  cout << 1;  
+  }
   // 4의 배수가 아닌 수
   else  cout << 0;
 }

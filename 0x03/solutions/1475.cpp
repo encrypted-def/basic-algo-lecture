aeb506673fb8509124520d9ddf8c51a4a7568130
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-08-18 01:46:53 +0900

    Update 1475.cpp

--- 0x03/solutions/1475.cpp
+++ 0x03/solutions/1475.cpp
@@ -1,23 +1,27 @@
-// Authored by : BaaaaaaaaaaarkingDog
-// Co-authored by : OceanShape
-// http://boj.kr/8b89557fe3194aaaba7cbf68ad02e7f7
+// Authored by : OceanShape
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/5bbbd57a699946759acf53355d886879
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(void) {
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int n, a[10]={}, ans=1;
-  cin >> n;
-  // 각 자릿수를 숫자 배열에 저장
-  for (int i=n; i>0; i/=10) a[i%10]++;
-  // 6과 9는 맞바꿀 수 있으므로, 평균을 사용해 배분
-  int tmp = a[6]+a[9];
-  a[6]=tmp/2;a[9]=tmp/2;
-  // 합이 홀수일 경우
-  // 정수형에서 평균을 구하면 1이 사라지므로 더해줌
-  if ((tmp/2)*2 != tmp) a[6]++;
-  // 가장 큰 값이 최소한으로 필요한 카드 세트 수
-  for (int i : a) ans = max(ans, i);
+  
+  // ans = 1로 둠으로서 N = 0일때에 대한 예외처리가 자연스럽게 가능해짐
+  int N, a[10] = {}, ans = 1;
+  cin >> N;
+  
+  // 자리수 추출
+  while(N){
+    a[N%10]++;
+    N /= 10;
+  }
+
+  for(int i = 0; i < 10; i++){
+    if(i == 6 || i == 9) continue;
+    ans = max(ans, a[i]);
+  }
+  // (a[6]+a[9])/2를 올림한 값이 6, 9에 대한 필요한 세트의 수이므로 (a[6]+a[9]+1)/2을 계산
+  ans = max(ans, (a[6]+a[9]+1)/2);
   cout << ans;
 }

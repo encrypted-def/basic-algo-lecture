f284057d09a1dabfe54633de84617c71cfb159c9
yoonsung.jung <ybook2012@gmail.com>
2021-10-01 03:05:08 +0900

    Update 7570.cpp

--- 0x11/solutions/7570.cpp
+++ 0x11/solutions/7570.cpp
@@ -1,11 +1,29 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : unluckyjung
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/d3653e0ab40644c29b02d67ec754f8d1
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main(void){
-  ios::sync_with_stdio(0);
+const int MAX = 1e6 + 2;
+
+int dp[MAX];
+int studentCount, studentNumber;
+int chainMaxLIS;
+
+int main() {
+  ios_base::sync_with_stdio(false);
   cin.tie(0);
   
+  cin >> studentCount;
+
+  for (int i = 0; i < studentCount; ++i) {
+    cin >> studentNumber;
+    dp[studentNumber] = dp[studentNumber - 1] + 1;
+    chainMaxLIS = max(chainMaxLIS, dp[studentNumber]);
+  }
+
+  cout << studentCount - chainMaxLIS << "\n";
+
+  return 0;
 }

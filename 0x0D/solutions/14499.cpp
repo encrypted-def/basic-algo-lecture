f7650bad431b34440b57b713f9864ba3ec96756e
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-10-01 13:36:12 +0900

    Update 14499.cpp

--- 0x0D/solutions/14499.cpp
+++ 0x0D/solutions/14499.cpp
@@ -1,6 +1,6 @@
 // Authored by : OceanShape
-// Co-authored by : -
-// http://boj.kr/2a631d2c79e64cef88cae03091a4ab4f
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/f4d1f744e8284036b39106a41f13ffe2
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,9 +13,17 @@ int dice[7];        // 주사위
 //     [3]
 //     [4]
 
+int idx[5][4] = {
+  {},        // dummy
+  {2,6,4,5}, // 동쪽, 5->2, 2->6, 6->4, 4->5
+  {2,5,4,6}, // 서쪽, 6->2, 2->5, 5->4, 4->6
+  {3,2,1,4}, // 북쪽, 4->3, 3->2, 2->1, 1->4
+  {2,3,4,1}, // 남쪽, 1->2, 2->3, 3->4, 4->1
+};
+
 // 명령어가 유효한지 검증하는 함수
 bool isOk(int nx, int ny){
-  if(nx<1||nx>N||ny<1||ny>M) return false;
+  if(nx<0||nx>=N||ny<0||ny>=M) return false;
   return true;
 }
 
@@ -25,36 +33,10 @@ void roll(int com){
   int tmp[7];
   for(int i = 1; i <= 6; ++i) tmp[i]=dice[i];
 
-  if(com==1){
-    // 동쪽
-    // 5->2, 2->6, 6->4, 4->5
-    tmp[2]=dice[5];
-    tmp[6]=dice[2];
-    tmp[4]=dice[6];
-    tmp[5]=dice[4];
-  } else if(com==2){
-    // 서쪽
-    // 6->2, 2->5, 5->4, 4->6
-    tmp[2]=dice[6];
-    tmp[5]=dice[2];
-    tmp[4]=dice[5];
-    tmp[6]=dice[4];
-  } else if(com==3){
-    // 북쪽
-    // 4->3, 3->2, 2->1, 1->4
-    tmp[3]=dice[4];
-    tmp[2]=dice[3];
-    tmp[1]=dice[2];
-    tmp[4]=dice[1];
-  } else{
-    // 남쪽
-    // 1->2, 2->3, 3->4, 4->1
-    tmp[2]=dice[1];
-    tmp[3]=dice[2];
-    tmp[4]=dice[3];
-    tmp[1]=dice[4];
-  }
-  
+  // 굴리는거 처리
+  for(int i = 0; i < 4; i++)
+    tmp[idx[com][i]] = dice[idx[com][(i+1)%4]];
+
   // 회전 결괏값을 기존 주사위에 대입
   for(int i = 1; i <= 6; ++i) dice[i]=tmp[i];
 }
@@ -71,9 +53,9 @@ void score(int c){
     x=nx; y=ny; // 유효성 확인 후 주사위의 위치 대입
     roll(c);
     // 0일 경우
-    if(board[nx][ny]==0){
+    if(board[nx][ny]==0)
       board[nx][ny]=dice[4]; // 칸에 바닥면 값 대입
-    }
+    
     // 0이 아닐 경우
     else {
       dice[4]=board[nx][ny]; // 바닥면에 칸 값 대입
@@ -87,9 +69,8 @@ int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   cin >> N >> M >> x >> y >> K;
-  x++; y++; // 원점이 (0,0)이므로 +1로 보정
-  for(int i = 1; i <= N; ++i)
-    for(int j = 1; j <= M; ++j)
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < M; ++j)
       cin >> board[i][j];
 
   while(K--){

5e2678f79de442cb224203d4776d86c2c71effaa
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-10-05 01:57:17 +0900

    Update 13335.cpp

--- 0x0D/solutions/13335.cpp
+++ 0x0D/solutions/13335.cpp
@@ -1,11 +1,11 @@
 // Authored by : OceanShape
-// Co-authored by : -
-// http://boj.kr/d51c2bc3daaa422a9e30463512174f9b
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/d0ab2b9645774292a86f9eee2a394171
 #include <bits/stdc++.h>
 using namespace std;
 
 int n, w, L, ans;
-int bridge[1001]; // 다리의 칸별 무게(트럭의 무게)를 저장하는 변수
+int bridge[101]; // 다리의 칸별 무게(트럭의 무게)를 저장하는 변수
 queue<int> truck; // 이동하는 트럭의 목록을 순차적으로 저장하는 변수
 
 // 다리가 비었는지 확인하는 함수
@@ -17,12 +17,9 @@ bool isEmpty(){
 
 // 트럭의 이동을 진행하는 함수
 void go(){
-  // 기존 트럭의 정보를 보존 후 덮의 씌움
-  int tmp[1001];
-  for(int i = 0; i<w-1; ++i)
-    tmp[i+1]=bridge[i];
-  for(int i = 0; i<w; ++i)
-    bridge[i]=tmp[i];
+  for(int i = w-1; i > 0; --i)
+    bridge[i] = bridge[i-1];
+  bridge[0] = 0;
 }
 
 // 다리 위 트럭의 무게를 계산하는 함수
@@ -51,7 +48,7 @@ int main(void){
       // 추가로 이동할 트럭이 있고, 다리가 무게를 버틸 경우
       if(!truck.empty()&&(tmp+truck.front()<=L)){
         bridge[0]=truck.front(); truck.pop();
-      } else bridge[0]=0;
+      }
     }
     ++ans;
   }while(!isEmpty()); // 모든 트럭이 이동하여 다리가 빌 때까지 반복

a75e7c8988dcc918ab4d702b4a229f18c3b13580
WonDongGyu <windowdong11@naver.com>
2021-10-24 01:30:53 +0900

    Update 2206.cpp

--- 0x09/solutions/2206.cpp
+++ 0x09/solutions/2206.cpp
@@ -1,11 +1,75 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : windowdong11
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/9c8a4f4d1c9146dc93118490764dd391
 #include <bits/stdc++.h>
 using namespace std;
+#define X first
+#define Y second
+typedef long long ll;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
+template <class Ty, class Tx>
+bool isInArea2d(pair<Ty, Tx> cur, pair<Ty, Tx> minSize, pair<Ty, Tx> maxSize) {
+  return minSize.first <= cur.first && cur.first < maxSize.first
+    && minSize.second <= cur.second && cur.second < maxSize.second;
+}
+
+pair<int,int> ways4[] = {
+    {0, 1},
+    {1, 0},
+    {0, -1},
+    {-1, 0},
+};
+
+char board[1000][1000];
+pair<int,int> boardEnd;
+int cost[1000][1000][2];
+// cost[x][y][0] : 벽을 하나도 부수지 않고 (x,y)까지 오는데 걸리는 비용
+// cost[x][y][1] : 벽을 하나만 부수고 (x,y)까지 오는데 걸리는 비용, (x,y)가 벽이라서 부수는 경우 포함
+int n, m;
+
+int bfs() {
+  queue<pair<bool, pair<int,int>>> q;
+  q.push({ false, {0, 0} });
+  while (!q.empty()) {
+    bool broken = q.front().first;
+    pair<int,int> cur = q.front().second;
+    int nextCost = cost[cur.X][cur.Y][broken] + 1;
+    q.pop();
+    for (int i = 0; i < 4; ++i) {
+      pair<int, int> next = { cur.X + ways4[i].X, cur.Y + ways4[i].Y };
+      if (next == boardEnd) return nextCost;
+      if (isInArea2d(next, { 0, 0 }, { m, n })) {
+        if (broken) {
+          if (board[next.X][next.Y] == '0' && cost[next.X][next.Y][1] == -1) {
+            cost[next.X][next.Y][1] = nextCost;
+            q.push({ 1, next });
+          }
+        }
+        else {
+          if (board[next.X][next.Y] == '0' && cost[next.X][next.Y][0] == -1) {
+            cost[next.X][next.Y][0] = cost[next.X][next.Y][1] = nextCost;
+            q.push({ 0, next });
+          }
+          else if (board[next.X][next.Y] == '1' && cost[next.X][next.Y][1] == -1) {
+            cost[next.X][next.Y][1] = nextCost;
+            q.push({ 1, next });
+          }
+        }
+      }
+    }
+  }
+  return -1;
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+  cin >> n >> m;
+  boardEnd = { m - 1, n - 1 };
+  for (int i = 0; i < n; ++i) for(int j = 0; j < m; ++j) cin >> board[j][i];
+  for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cost[j][i][0] = cost[j][i][1] = -1;
+  cost[0][0][0] = cost[0][0][1] = 1;
+  if (n == 1 && m == 1) cout << 1;
+  else cout << bfs();
+  return 0;
 }

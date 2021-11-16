75c0454c05bdaae524bca193412700072c4cca87
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-11-16 15:00:34 +0900

    Update 5373.cpp

--- 0x0D/solutions/5373.cpp
+++ 0x0D/solutions/5373.cpp
@@ -1,19 +1,18 @@
 // Authored by : windowdong11
-// Co-authored by : -
-// http://boj.kr/0350797626e0424db3de3235df4cd01a
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/31c7548b7f964e70b774e9fb393f34bf
 #include <bits/stdc++.h>
 using namespace std;
 
 char cube[6][3][3];
 char color[] = {'w', 'y', 'r', 'o', 'g', 'b'};
-enum side{
-  up = 0,
-  down,
-  front,
-  back,
-  left,
-  right,
-};
+
+const int UP = 0;
+const int DOWN = 1;
+const int FRONT = 2;
+const int BACK = 3;
+const int LEFT = 4;
+const int RIGHT = 5;
 
 void initGlobal(){
   for(int s = 0; s < 6; ++s)
@@ -21,147 +20,83 @@ void initGlobal(){
       for(int j = 0; j < 3; ++j)
         cube[s][i][j] = color[s];
 }
+
 void rotate(int side, bool isclockwise){
-  char tmp[3][3];
-  if(isclockwise)
+  int rot = 1;
+  if(!isclockwise) rot = 3;
+  while(rot--){
+    char tmp[3][3];
     for(int i = 0; i < 3; ++i)
       for(int j = 0; j < 3; ++j)
         tmp[j][2 - i] = cube[side][i][j];
-  else 
-    for(int i = 0; i < 3; ++i)
-        for(int j = 0; j < 3; ++j)
-          tmp[2 - j][i] = cube[side][i][j];
-  swap(cube[side], tmp);
+    swap(cube[side], tmp);
+  }
 }
 
 void rotateside(int side, bool isclockwise){
   rotate(side, isclockwise);
+  int rot = 1;
+  if(!isclockwise) rot = 3;
   int tmp;
-  if(side == side::front){
-    if(isclockwise){
+  while(rot--){
+    if(side == FRONT){
       for(int i = 0; i < 3; ++i){
-        tmp = cube[side::up][2][i];
-        cube[side::up][2][i] = cube[side::left][2 - i][2];
-        cube[side::left][2 - i][2] = cube[side::down][0][2 - i];
-        cube[side::down][0][2 - i] = cube[side::right][i][0];
-        cube[side::right][i][0] = tmp;
+        tmp = cube[UP][2][i];
+        cube[UP][2][i] = cube[LEFT][2 - i][2];
+        cube[LEFT][2 - i][2] = cube[DOWN][0][2 - i];
+        cube[DOWN][0][2 - i] = cube[RIGHT][i][0];
+        cube[RIGHT][i][0] = tmp;
       }
     }
-    else {
+    else if(side == BACK){
       for(int i = 0; i < 3; ++i){
-        tmp = cube[side::up][2][i];
-        cube[side::up][2][i] = cube[side::right][i][0];
-        cube[side::right][i][0] = cube[side::down][0][2 - i];
-        cube[side::down][0][2 - i]= cube[side::left][2 - i][2];
-        cube[side::left][2 - i][2] = tmp;
+        tmp = cube[UP][0][i];
+        cube[UP][0][i] = cube[RIGHT][i][2];
+        cube[RIGHT][i][2] = cube[DOWN][2][2 - i];
+        cube[DOWN][2][2 - i] = cube[LEFT][2 - i][0];
+        cube[LEFT][2 - i][0] = tmp;
       }
     }
-  }
-  else if(side == side::back){
-    if(isclockwise){
+    else if(side == LEFT){
       for(int i = 0; i < 3; ++i){
-        tmp = cube[side::up][0][i];
-        cube[side::up][0][i] = cube[side::right][i][2];
-        cube[side::right][i][2] = cube[side::down][2][2 - i];
-        cube[side::down][2][2 - i] = cube[side::left][2 - i][0];
-        cube[side::left][2 - i][0] = tmp;
+        tmp = cube[UP][i][0];
+        cube[UP][i][0] = cube[BACK][i][0];
+        cube[BACK][i][0]  = cube[DOWN][i][0];
+        cube[DOWN][i][0] = cube[FRONT][i][0];
+        cube[FRONT][i][0] = tmp;
       }
     }
-    else {
+    else if(side == RIGHT){
       for(int i = 0; i < 3; ++i){
-        tmp = cube[side::up][0][i];
-        cube[side::up][0][i] = cube[side::left][2 - i][0];
-        cube[side::left][2 - i][0] = cube[side::down][2][2 - i];
-        cube[side::down][2][2 - i] = cube[side::right][i][2];
-        cube[side::right][i][2] = tmp;
+        tmp = cube[UP][i][2];
+        cube[UP][i][2] = cube[FRONT][i][2];
+        cube[FRONT][i][2] = cube[DOWN][i][2];
+        cube[DOWN][i][2] = cube[BACK][i][2];
+        cube[BACK][i][2] = tmp;
       }
     }
-  }
-  else if(side == side::left){
-    if(isclockwise){
+    else if(side == UP){
       for(int i = 0; i < 3; ++i){
-        tmp = cube[side::up][i][0];
-        cube[side::up][i][0] = cube[side::back][i][0];
-        cube[side::back][i][0]  = cube[side::down][i][0];
-        cube[side::down][i][0] = cube[side::front][i][0];
-        cube[side::front][i][0] = tmp;
-
+        tmp = cube[FRONT][0][i];
+        cube[FRONT][0][i] = cube[RIGHT][0][i];
+        cube[RIGHT][0][i] = cube[BACK][2][2 - i];
+        cube[BACK][2][2 - i] = cube[LEFT][0][i];
+        cube[LEFT][0][i] = tmp;
       }
     }
-    else {
+    else{ // DOWN
       for(int i = 0; i < 3; ++i){
-        tmp = cube[side::up][i][0];
-        cube[side::up][i][0] = cube[side::front][i][0];
-        cube[side::front][i][0] = cube[side::down][i][0];
-        cube[side::down][i][0] = cube[side::back][i][0];
-        cube[side::back][i][0] = tmp;
-      }
-    }
-  }
-  else if(side == side::right){
-    if(isclockwise){
-      for(int i = 0; i < 3; ++i){
-        tmp = cube[side::up][i][2];
-        cube[side::up][i][2] = cube[side::front][i][2];
-        cube[side::front][i][2] = cube[side::down][i][2];
-        cube[side::down][i][2] = cube[side::back][i][2];
-        cube[side::back][i][2] = tmp;
-      }
-    }
-    else {
-      for(int i = 0; i < 3; ++i){
-        tmp = cube[side::up][i][2];
-        cube[side::up][i][2] = cube[side::back][i][2];
-        cube[side::back][i][2]  = cube[side::down][i][2];
-        cube[side::down][i][2] = cube[side::front][i][2];
-        cube[side::front][i][2] = tmp;
-      }
-    }
-  }
-  else if(side == side::up){
-    if(isclockwise){
-      for(int i = 0; i < 3; ++i){
-        tmp = cube[side::front][0][i];
-        cube[side::front][0][i] = cube[side::right][0][i];
-        cube[side::right][0][i] = cube[side::back][2][2 - i];
-        cube[side::back][2][2 - i] = cube[side::left][0][i];
-        cube[side::left][0][i] = tmp;
-      }
-    }
-    else {
-      for(int i = 0; i < 3; ++i){
-        tmp = cube[side::front][0][i];
-        cube[side::front][0][i] = cube[side::left][0][i];
-        cube[side::left][0][i] = cube[side::back][2][2 - i];
-        cube[side::back][2][2 - i] = cube[side::right][0][i];
-        cube[side::right][0][i] = tmp;
-      }
-    }
-  }
-  else if(side == side::down){
-    if(isclockwise){
-      for(int i = 0; i < 3; ++i){
-        tmp = cube[side::front][2][i];
-        cube[side::front][2][i] = cube[side::left][2][i];
-        cube[side::left][2][i] = cube[side::back][0][2 - i];
-        cube[side::back][0][2 - i] = cube[side::right][2][i];
-        cube[side::right][2][i] = tmp;
-      }
-    }
-    else {
-      for(int i = 0; i < 3; ++i){
-        tmp = cube[side::front][2][i];
-        cube[side::front][2][i] = cube[side::right][2][i];
-        cube[side::right][2][i] = cube[side::back][0][2 - i];
-        cube[side::back][0][2 - i] = cube[side::left][2][i];
-        cube[side::left][2][i] = tmp;
+        tmp = cube[FRONT][2][i];
+        cube[FRONT][2][i] = cube[LEFT][2][i];
+        cube[LEFT][2][i] = cube[BACK][0][2 - i];
+        cube[BACK][0][2 - i] = cube[RIGHT][2][i];
+        cube[RIGHT][2][i] = tmp;
       }
     }
   }
 }
 
-int main()
-{
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
   int t;
@@ -173,33 +108,24 @@ int main()
     string s;
     while(n--){
       cin >> s;
-      switch (s[0])
-      {
-      case 'F':
-        rotateside(side::front, s[1] == '+');
-        break;
-      case 'B':
-        rotateside(side::back, s[1] == '+');
-        break;
-      case 'U':
-        rotateside(side::up, s[1] == '+');
-        break;
-      case 'D':
-        rotateside(side::down, s[1] == '+');
-        break;
-      case 'L':
-        rotateside(side::left, s[1] == '+');
-        break;
-      case 'R':
-        rotateside(side::right, s[1] == '+');
-        break;
-      }
+      if(s[0] == 'F') rotateside(FRONT, s[1] == '+');
+      else if(s[0] == 'B') rotateside(BACK, s[1] == '+');
+      else if(s[0] == 'U') rotateside(UP, s[1] == '+');
+      else if(s[0] == 'D') rotateside(DOWN, s[1] == '+');
+      else if(s[0] == 'L') rotateside(LEFT, s[1] == '+');
+      else rotateside(RIGHT, s[1] == '+');
     }
     for(int i = 0; i < 3; ++i){
       for(int j = 0; j < 3; ++j)
-        cout << cube[up][i][j];
+        cout << cube[UP][i][j];
       cout << '\n';
     }
   }
   return 0;
 }
+
+/*
+매번 회전이 발생했을 때 칸에 적힌 값들이 어떻게 변화하는지를 잘 계산해서 넣어줘야 한다.
+시계 방향, 반시계 방향을 다 구현하는 대신 반시계 방향은 시계 방향 회전을 3번 하는
+방법으로 구현하면 구현량을 조금이나마 줄일 수 있다.
+*/

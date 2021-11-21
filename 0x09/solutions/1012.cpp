8570020b290fc44a4e00c53d3b38f398c4884e17
seeys <es2008e@naver.com>
2021-11-21 16:04:41 +0900

    Update 1012.cpp

--- 0x09/solutions/1012.cpp
+++ 0x09/solutions/1012.cpp
@@ -1,11 +1,56 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : seeys
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/032a4aa61296423d99d2af6d31a4b037
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(void){
+#define X first
+#define Y second
+int board[51][51];
+bool visited[51][51];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int m, n, k;
+queue <pair<int, int >> q;
+void bfs(int x, int y) {
+  q.push({ x,y });
+  while (!q.empty()) {
+    auto cur = q.front(); q.pop();
+    visited[x][y] = true;
+    for (int dir = 0; dir < 4; dir++) {
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if (visited[nx][ny] || board[nx][ny] != 1) continue;
+      visited[nx][ny] = true;
+      q.push({ nx,ny });
+    }
+  }
+}
+int main(void) 
+{
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  int t;
+  cin >> t;
+  while (t--) {
+    cin >> m >> n >> k;
+    int x, y;
+    for (int i = 0; i < k; i++) {
+      cin >> x >> y;
+      board[y][x] = 1;
+    }
+    int res = 0; //지렁이 개수
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (board[i][j] == 1 && !visited[i][j]) {
+          bfs(i, j);
+          res++;
+        }
+      }
+    }
+    cout << res << "\n";
+    memset(board, 0, sizeof(board));
+    memset(visited, false, sizeof(visited));
+  }
+  return 0;
 }

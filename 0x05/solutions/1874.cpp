0426ab465e074b7242991b9d43a2418e276f5f01
Haneul Kim <68045288+haneulkimdev@users.noreply.github.com>
2021-11-21 23:11:45 +0900

    Update 1874.cpp

--- 0x05/solutions/1874.cpp
+++ 0x05/solutions/1874.cpp
@@ -1,11 +1,31 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : haneulkimdev
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/5926f973a19c4bf398c573658842e198
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  int n;
+  cin >> n;
+  stack<int> S;
+  int cnt = 1;
+  string ans;
+  while (n--) {
+    int t;
+    cin >> t;
+    while (cnt <= t) {
+      S.push(cnt++);
+      ans += "+\n";
+    }
+    if (S.top() == t) {
+      S.pop();
+      ans += "-\n";
+    }
+  }
+  if (S.empty())
+    cout << ans;
+  else
+    cout << "NO";
 }

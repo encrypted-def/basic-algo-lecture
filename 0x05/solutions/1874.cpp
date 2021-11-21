497b662615831f5f29a89cd0c37cedfa5e422c6f
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-11-21 23:36:55 +0900

    Update 1874.cpp

--- 0x05/solutions/1874.cpp
+++ 0x05/solutions/1874.cpp
@@ -1,6 +1,6 @@
 // Authored by : haneulkimdev
-// Co-authored by : -
-// http://boj.kr/5926f973a19c4bf398c573658842e198
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/7a18dc82bd4041aaa59c745f06d4ba60
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -19,13 +19,18 @@ int main(void) {
       S.push(cnt++);
       ans += "+\n";
     }
-    if (S.top() == t) {
-      S.pop();
-      ans += "-\n";
+    if (S.top() != t) {
+      cout << "NO\n";
+      return 0;
     }
+    S.pop();
+    ans += "-\n";
   }
-  if (S.empty())
-    cout << ans;
-  else
-    cout << "NO";
+  cout << ans;
 }
+
+/*
+현재 처리를 필요로 하는 수는 cnt이다. cnt 이상인 값 t가 주어지면 그 값을 pop할 수 있게 cnt가 t가
+될 때 까지 push를 해야 한다(18-21줄). 만약 22번째 줄과 같이 S.top()과 t가 일치하지 않는다면
+올바르지 않은 수열이다.
+*/

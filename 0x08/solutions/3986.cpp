16e258ddd846ac48833f54d8e4742448f1d9d1b6
yongjunleeme <chesidyong@gmail.com>
2021-11-22 00:44:01 +0900

    Update 3986.cpp

--- 0x08/solutions/3986.cpp
+++ 0x08/solutions/3986.cpp
@@ -1,11 +1,31 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : yongjunleeme
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/0aaffc76df1f4dd4872b53a3f1b3b277
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int n, ans = 0;
+
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> n;
+  while (n--){
+    string a;
+   cin >> a;
+   stack <int> S;
+
+   for (auto c: a){
+     if (!S.empty()){
+       if (S.top() == 'A' && c == 'A') S.pop();
+       else if (S.top() == 'B' && c == 'B') S.pop();
+        else S.push(c);
+      }
+      else S.push(c);
+   }
+   if (S.empty()) ans++;
+  }
+
+  cout << ans << "\n";
 }

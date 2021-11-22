17854b73d683c77975b26b438d804fbd1dccc62a
yongjunleeme <chesidyong@gmail.com>
2021-11-22 22:52:49 +0900

    Update 7562.cpp
    안녕하세요 저는 바킹독님 팬입니다. 첫 PR에 코멘트와 코드수정까지 해주셔서 매우 감사합니다.
    띄어쓰기를 틀리다니.. 저는 맥을 쓰는데 C++ IDE를 잘 몰라서 [OnlineGDB](https://www.onlinegdb.com)를 이용했습니다.
    복붙하면서 띄어쓰기를 틀렸던 것 같습니다 ㅠㅠ 이번에는 검색한 C++ Formatter 툴을 사용해서 PR을 드리는데 올바른지 모르겠네요;
    큰 기여는 아니지만 그래도 개인적으로 기부니가 좋습니다. 멋진 강의 다시 한 번 감사드리며 행복한 연말보내시길 바라옵니다.

--- 0x09/solutions/7562.cpp
+++ 0x09/solutions/7562.cpp
@@ -1,11 +1,48 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : yongjunleeme
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/4bdeb8c2571e473b8dc0d74785f19d5a
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+#define X first
+#define Y second
+int dist[305][305];
+int dx[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+int dy[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+int t, n, x, y, xx, yy;
+queue <pair<int, int >> Q;
+
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    for (int i = 0; i < n; i++) fill(dist[i], dist[i] + n, -1);
+    cin >> x >> y;
+    dist[x][y] = 0;
+    Q.push({x, y});
+    cin >> xx >> yy;
+    if (x == xx && y == yy) {
+      cout << 0 << "\n";
+      continue;
+    }
+    while (!Q.empty()) {
+      auto cur = Q.front(); Q.pop();
+      for (int dir = 0; dir < 8; dir++) {
+        int nx = cur.X + dx[dir];
+        int ny = cur.Y + dy[dir];
+        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+        if (dist[nx][ny] >= 0) continue;
+        dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+        if (nx == xx && ny == yy) {
+          cout << dist[nx][ny] << "\n";
+          continue;
+        }
+        Q.push({nx, ny});
+      }
+    }
+  }
 }
+

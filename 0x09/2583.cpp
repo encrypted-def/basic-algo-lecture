09a0860158341520773e186a9895e6476bd50643
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-11-25 13:11:20 +0900

    Update 2583.cpp

--- 0x09/2583.cpp
+++ 0x09/2583.cpp
@@ -1,46 +1,43 @@
 // Authored by : std-freejia
-// Co-authored by :
-// http://boj.kr/15af16c30a0d4c519eb9ec8a5ebc7475
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/f34eee0ca14f450c9ad0a0e9deefc033
 #include <bits/stdc++.h>
-#define X first;
-#define Y second;
 using namespace std;
 
-int  m, n, k;
+#define X first
+#define Y second
+
+int m, n, k;
 int a, b, c, d;
 int board[102][102];
 vector<int> v;
 int dx[] = {0, 1, 0, -1};
 int dy[] = {1, 0, -1, 0};
 
-void fillBoard(int a, int b, int c, int d){ // -> (b,a) (d,c)
-  for(int i = b; i < d; i++){
-    for(int j = a; j < c; j++) board[i][j] = 1; // 직사각형 내부 1 표시
-  }
+void fillBoard(int a, int b, int c, int d){
+  for(int i = a; i < c; i++)
+    fill(board[i]+b, board[i]+d, 1); // 직사각형 내부 1 표시
 }
 
-int bfs(int a, int b){
-
+int bfs(int i, int j){
   int cnt = 0;
   queue<pair<int,int>> q;
-  board[a][b] = 1; // 방문
-  q.push({a,b}); // 푸시
+  board[i][j] = 1; // 방문
+  q.push({i,j}); // 푸시
 
   while(!q.empty()){
-    int x = q.front().X;
-    int y = q.front().Y;
-    q.pop();
+    auto cur = q.front(); q.pop();
     cnt++;
 
     for(int i = 0; i < 4; i++){
-      int tempX = x + dx[i];
-      int tempY = y + dy[i];
+      int nx = cur.X + dx[i];
+      int ny = cur.Y + dy[i];
 
-      if(tempX < 0 || tempX >= m || tempY < 0 || tempY >= n) continue;
-      if(board[tempX][tempY] == 1) continue;
+      if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+      if(board[nx][ny] == 1) continue;
       else{
-        board[tempX][tempY] = 1;  // 첫 방문이라면 방문표시
-        q.push({tempX, tempY}); // 큐에 넣기
+        board[nx][ny] = 1;  // 첫 방문이라면 방문표시
+        q.push({nx, ny}); // 큐에 넣기
       }
     }
   }
@@ -58,8 +55,8 @@ int main(void) {
     fillBoard(a, b, c, d);
   }
 
-  for(int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) { // 0인 영역에서 시작하기
+  for(int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) { // 0인 영역에서 시작하기
       if (!board[i][j])
         v.push_back(bfs(i, j));
     }
@@ -70,3 +67,10 @@ int main(void) {
 
   return 0;
 }
+
+/*
+문제에서 주어진 좌표의 방향을 무시하고 (x,y)에 대해
+(0,0) (0,1) (0,2) ...
+(1,0) (1,1) (1,2) ...
+으로 배치함. 이 경우 행은 n개, 열은 m개인 n by m 배열이 됨.
+*/

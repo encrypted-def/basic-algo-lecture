034df054dd446c6446fc39bd52b18a67ba261d24
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-11-26 11:19:33 +0900

    Update 2468.cpp

--- 0x09/solutions/2468.cpp
+++ 0x09/solutions/2468.cpp
@@ -1,6 +1,6 @@
 // Authored by : std-freejia
-// Co-authored by : -
-// http://boj.kr/467b23760a2e4a378e1c1fce14ed8d44
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/4b36ea477ba8433c9cf9d053a711e07b
 #include <bits/stdc++.h>
 #define X first
 #define Y second
@@ -12,21 +12,16 @@ int vis[102][102]; // 비에 잠긴 영역
 int dx[] = {0, 1, 0, -1};
 int dy[] = {1, 0, -1, 0};
 
-void bfs(int x, int y, int limit){
-
+void bfs(int i, int j, int limit){
   queue<pair<int,int>> q;
-
-  vis[x][y] = 1; // 방문표시
-  q.push({x,y}); // 푸시
-
+  vis[i][j] = 1; // 방문표시
+  q.push({i, j}); // 푸시
   while(!q.empty()){
-    int xx = q.front().X;
-    int yy = q.front().Y;
-    q.pop();
+    auto cur = q.front(); q.pop();
 
     for(int i = 0; i < 4; i++){
-      int nx = xx + dx[i];
-      int ny = yy + dy[i];
+      int nx = cur.X + dx[i];
+      int ny = cur.Y + dy[i];
 
       if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue; // 정상 범위 체크
       if(vis[nx][ny] == 0 && area[nx][ny] > limit){  // 미방문, 침수되지 않음
@@ -42,7 +37,6 @@ int main(void) {
   cin.tie(0);
 
   cin >> n;
-
   for(int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) { // 0인 영역에서 시작하기
       cin >> area[i][j];
@@ -51,21 +45,19 @@ int main(void) {
   }
 
   for(int limit = 0; limit <= maxlimit; limit++) {
-    for(int i = 0; i < n; i++) fill(vis[i], vis[i]+n, 0); // 방문배열 초기화
+    for(int i = 0; i < n; i++)
+      fill(vis[i], vis[i]+n, 0); // 방문배열 초기화
+    
     int cnt = 0;
-
-    for(int a = 0; a < n; a++) {
-      for (int b = 0; b < n; b++) {
-        if (area[a][b] > limit && vis[a][b] == 0) { // 침수 여부, 방문 여부
-          bfs(a, b, limit);
+    for(int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (area[i][j] > limit && vis[i][j] == 0) { // 침수 여부, 방문 여부
+          bfs(i, j, limit);
           cnt++;
         }
       }
     }
     maxcnt = max(cnt, maxcnt);
   }
-
   cout << maxcnt;
-
-  return 0;
 }

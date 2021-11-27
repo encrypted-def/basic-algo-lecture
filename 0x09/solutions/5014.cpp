df8ca024279cdebb4bea59db7a47cf65d20b2cdc
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-11-27 11:06:20 +0900

    Update 5014.cpp

--- 0x09/solutions/5014.cpp
+++ 0x09/solutions/5014.cpp
@@ -1,54 +1,31 @@
 // Authored by : std-freejia
-// Co-authored by : -
-// http://boj.kr/b964314a11544ddba420db6f3dab1f22
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/8f34874217304e3ca3a499f672788d70
 #include <bits/stdc++.h>
-#define X first
-#define Y second
 using namespace std;
 
-int f, s, g, u, d, answer;
-int vis[1000002];
-bool flag = false;
-
-void bfs(){
-
-  queue<pair<int,int> > qu; // 현재 층, 버튼 클릭 수
-  vis[s] = 1; // 현재 위치 방문
-  qu.push({s, 0}); // 0 번 클릭
-
-  while(!qu.empty()){
-    int curs = qu.front().X;
-    int curcnt = qu.front().Y;
-    qu.pop();
-
-    if(curs == g) {
-      flag = true;
-      answer = curcnt;
-      break;
-    }
-
-    int nexts[2] = {curs + u, curs - d};
-
-    for(auto next : nexts){
-      if(next <= f && next > 0 && vis[next] == 0){ // 존재하는 층 범위 및 방문 여부 확인
-        vis[next] = 1;
-        qu.push({next, curcnt+1});
-      }
-    }
-  }
-
-}
+int f, s, g, u, d;
+int dist[1000002];
 
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin >> f >> s >> g >> u >> d;
+  fill(dist+1, dist+f+1, -1);
+  
+  queue<int> q;
+  dist[s] = 0; // 현재 위치의 거리를 0으로 둠
+  q.push(s); // s층에서 시작
+  while(!q.empty()){
+    int cur = q.front(); q.pop();
+    for(auto nxt : {cur + u, cur - d}){
+      if(nxt > f || nxt <= 0 || dist[nxt] != -1) continue;
+      dist[nxt] = dist[cur] + 1;
+      q.push(nxt);
+    }
+  }
 
-  bfs();
-
-  if(flag) cout << answer;
-  else cout << "use the stairs";
-
-  return 0;
+  if(dist[g] == -1) cout << "use the stairs";
+  else cout << dist[g];
 }

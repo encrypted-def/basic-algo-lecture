1e7b6e836f77c51ec31c2d8f432140f8d2970dc4
Haneul Kim <95077661+haneulkimdev@users.noreply.github.com>
2021-11-30 22:58:43 +0900

    Update 9012.cpp

--- 0x08/solutions/9012.cpp
+++ 0x08/solutions/9012.cpp
@@ -1,11 +1,34 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : haneulkimdev
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/1731ef56b7594c7f99972859d2d979bb
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  int n;
+  cin >> n;
+  while (n--) {
+    string a;
+    cin >> a;
+    stack<char> s;
+    bool is_valid = true;
+    for (auto c : a) {
+      if (c == '(') {
+        s.push(c);
+      } else if (c == ')') {
+        if (s.empty() || s.top() != '(') {
+          is_valid = false;
+          break;
+        }
+        s.pop();
+      }
+    }
+    if (!s.empty()) is_valid = false;
+    if (is_valid)
+      cout << "YES\n";
+    else
+      cout << "NO\n";
+  }
 }

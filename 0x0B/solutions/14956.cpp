cfe1f300971f2d48cd1134e6639d5b4e5da53a42
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-12-15 20:00:03 +0900

    Update 14956.cpp

--- 0x0B/solutions/14956.cpp
+++ 0x0B/solutions/14956.cpp
@@ -1,15 +1,13 @@
 // Authored by : heheHwang
-// Co-authored by : -
-// http://boj.kr/bb5866b211a7402f81f4654196240b41
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/79abb80d66324ff0afb4bb5698c8133f
 #include <bits/stdc++.h>
 using namespace std;
 
 #define X first
 #define Y second
-using ll = long long;
-using PLL = pair<ll, ll>;
 // 변의 길이, 걸어간 거리로 x, y좌표값을 재귀적으로 계산
-PLL philo(ll side, ll walk) {
+pair<int,int> philo(int side, int walk) {
   // Base case: 변의 길이가 2^1인 경우
   if (side == 2) {
     switch (walk) {
@@ -23,19 +21,22 @@ PLL philo(ll side, ll walk) {
         return {2, 1};
     }
   }
-  ll half = side / 2, section = half * half;
+  int half = side / 2, section = half * half;
   // walk가 속하는 범위에 따라 계산
   if (walk <= section) {
-    PLL rv = philo(half, walk);
+    pair<int,int> rv = philo(half, walk);
     return {rv.Y, rv.X};
-  } else if (walk <= 2 * section) {
-    PLL rv = philo(half, walk - section);
+  }
+  else if (walk <= 2 * section) {
+    pair<int,int> rv = philo(half, walk - section);
     return {rv.X, half + rv.Y};
-  } else if (walk <= 3 * section) {
-    PLL rv = philo(half, walk - 2 * section);
+  }
+  else if (walk <= 3 * section) {
+    pair<int,int> rv = philo(half, walk - 2 * section);
     return {half + rv.X, half + rv.Y};
-  } else {
-    PLL rv = philo(half, walk - 3 * section);
+  }
+  else {
+    pair<int,int> rv = philo(half, walk - 3 * section);
     return {2 * half - rv.Y + 1, half - rv.X + 1};
   }
 }
@@ -44,9 +45,9 @@ int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  ll side, walk;
+  int side, walk;
   cin >> side >> walk;
-  PLL ans = philo(side, walk);
+  pair<int,int> ans = philo(side, walk);
   cout << ans.X << " " << ans.Y;
 }
 /*

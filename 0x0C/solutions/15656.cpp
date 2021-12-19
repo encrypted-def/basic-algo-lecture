81619f872bdab81114101e4f058b7af97e6ced6b
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-12-20 02:43:32 +0900

    Update 15656.cpp

--- 0x0C/solutions/15656.cpp
+++ 0x0C/solutions/15656.cpp
@@ -1,22 +1,23 @@
 // Authored by : yongjunleeme
-// Co-authored by : -
-// http://boj.kr/6caa5519520247128be896a27dbc46fa
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/8b9c11f721a44186bb4da3bff5e72f88
 #include <bits/stdc++.h>
 using namespace std;
-int n, m;
-vector<int> a;
-int ans[10];
 
-void func(int k){
-  if(k == m){
-    for(int i = 0; i < m; i++) cout << ans[i] << " ";
-    cout << "\n";
+int n, m;
+int arr[10];
+int num[10];
+
+void func(int k){ // 현재 k개까지 수를 택했음.
+  if(k == m){ // m개를 모두 택했으면
+    for(int i = 0; i < m; i++)
+      cout << num[arr[i]] << ' '; // arr에 기록해둔 인덱스를 참고해 값을 출력
+    cout << '\n';
     return;
   }
-
-  for(int i = 0; i < n; i++){
-    ans[k] = a[i];
-    func(k+1);
+  for(int i = 0; i < n; i++){ 
+    arr[k] = i; // k번째 수를 i로 정함
+    func(k+1); // 다음 수를 정하러 한 단계 더 들어감
   }
 }
 
@@ -25,10 +26,8 @@ int main(void){
   cin.tie(0);
   cin >> n >> m;
   for(int i = 0; i < n; i++){
-    int tmp;
-    cin >> tmp;
-    a.push_back(tmp);
+    cin >> num[i];
   }
-  sort(a.begin(), a.end());
+  sort(num, num+n);
   func(0);
 }

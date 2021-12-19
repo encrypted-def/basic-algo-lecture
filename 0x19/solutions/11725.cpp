3c16b3633f358de648509eb91745e4f238ce966c
Mincheol Son <20028331+encrypted-def@users.noreply.github.com>
2021-12-20 06:03:57 +0900

    0x19 tree

---
+++ 0x19/solutions/11725.cpp
@@ -0,0 +1,30 @@
+// http://boj.kr/ba2ccffa99514399aebf4bcc9e391a48
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> adj[100001];
+int p[100001];
+
+void dfs(int cur) {
+  for (int nxt : adj[cur]) {
+    if (p[cur] == nxt) continue;
+    p[nxt] = cur;
+    dfs(nxt);
+  }
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  for(int i = 0; i < n-1; i++){
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  dfs(1);
+  for(int i = 2; i <= n; i++) cout << p[i] << '\n';
+}

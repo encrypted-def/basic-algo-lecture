b61de283ba61f543beb4c00d8bc28ebb9eeb367e
std-freejia <wtfreejia@gmail.com>
2021-12-20 12:28:31 +0900

    Update 11650.cpp
    좌표정렬하기 문제 입니다

--- 0x0E/solutions/11650.cpp
+++ 0x0E/solutions/11650.cpp
@@ -1,11 +1,21 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : std-freejia
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/2606a61ca3804fbea9cdeb42f72632c4
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int n, a, b;
+vector<pair<int, int>> v;
+
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  cin >> n;
+  for(int i = 0; i < n; i++) {
+    cin >> a >> b;
+    v.push_back({a, b});
+  }
+  sort(v.begin(), v.end());
+  for(int i = 0; i < n; i++) cout << v[i].first << ' ' << v[i].second << '\n';
+  return 0;
 }

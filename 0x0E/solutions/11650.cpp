eafefadee57a4ed97fbbaee34da0f7ae42628ad1
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-12-21 02:14:13 +0900

    Update 11650.cpp

--- 0x0E/solutions/11650.cpp
+++ 0x0E/solutions/11650.cpp
@@ -1,21 +1,22 @@
 // Authored by : std-freejia
-// Co-authored by : -
-// http://boj.kr/2606a61ca3804fbea9cdeb42f72632c4
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/5a5a95bcc0644b138ad6850e5db9e87a
 #include <bits/stdc++.h>
 using namespace std;
 
+#define X first
+#define Y second
+
 int n, a, b;
-vector<pair<int, int>> v;
+pair<int, int> p[100004];
 
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
   cin >> n;
   for(int i = 0; i < n; i++) {
-    cin >> a >> b;
-    v.push_back({a, b});
+    cin >> p[i].X >> p[i].Y;
   }
-  sort(v.begin(), v.end());
-  for(int i = 0; i < n; i++) cout << v[i].first << ' ' << v[i].second << '\n';
-  return 0;
+  sort(p, p + n);
+  for(int i = 0; i < n; i++) cout << p[i].X << ' ' << p[i].Y << '\n';
 }

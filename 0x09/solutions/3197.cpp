ecf5e81c3c242de4d38dfa33dfa14894b5425e2c
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-12-21 02:33:25 +0900

    Update 3197.cpp

--- 0x09/solutions/3197.cpp
+++ 0x09/solutions/3197.cpp
@@ -1,12 +1,13 @@
 // Authored by : yongjunleeme
 // Co-authored by : BaaaaaaaaaaarkingDog
-// http://boj.kr/5cd7aca68a0844afb64b3ab9ca68c569
+// http://boj.kr/3ac8a6ad3fdf4ed3a5a88c75574c4d16
 #include <bits/stdc++.h>
 using namespace std;
 #define X first
 #define Y second
 
 const int MX = 1520;
+// lx, ly = 어떤 한 백조의 위치
 int r, c, lx, ly, cnt = 0;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
@@ -90,3 +91,15 @@ int main(void){
   }
   cout << cnt;
 }
+
+/*
+매번 O(RC)에 걸쳐 빙판을 녹이고 두 백조가 있는 곳이 연결되어 있는지 직접 BFS로 확인하는 풀이는
+걸리는 날이 최악의 경우 대략 O(max(R,C))정도 될 수 있으니 1500^3이 되어 시간 초과가 발생한다.
+
+대신 1. 얼음을 녹일 때 직전에 녹은 얼음 주변만 확인하면 된다(본문의 코드에서는 Q2를 이용해 처리)
+
+2. (lx, ly)에서 출발한 BFS의 경우 얼음을 마주할 경우 그 얼음은 다음날 녹기 때문에 그 얼음의 위치를
+임시로 기억하고 있다가 백조에 대한 BFS를 처리하는 LQ에 넣어주면 된다(본문의 코드에서는 LQ2를 이용해 처리) 
+
+이 2가지 사실을 관찰해서 BFS를 진행하면 O(RC)에 해결할 수 있다.
+*/

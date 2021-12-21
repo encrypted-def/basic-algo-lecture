3fcca992e413f318e5fe0c48565b5e3b22a7b31e
gunny6026 <gunny6026@naver.com>
2021-12-21 20:40:03 +0900

    update 15663.cpp

--- 0x0C/solutions/15663.cpp
+++ 0x0C/solutions/15663.cpp
@@ -1,11 +1,41 @@
 // Authored by : BaaaaaaaaaaarkingDog
-// Co-authored by : -
+// Co-authored by : - connieya
 // http://boj.kr/****************
 #include <bits/stdc++.h>
-using namespace std;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
+using namespace std;
+int n, m;
+int arr[10];
+int num[10];
+bool visited[10];
+
+void func(int L) {
+    if (L == m) {
+        for (int i = 0; i < m; ++i) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    int tmp =0; // 중복 수열인지 확인 하기 위해 필요한 임시 변수
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i] && tmp != num[i]){  // 이전 수열의 마지막 항과 새로운 수열의 마지막 항이 같으면 중복 수열
+            visited[i] = true;
+            arr[L] = num[i];
+            tmp = arr[L];
+            func(L + 1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i) {
+        cin >> num[i];
+    }
+    sort(num, num + n);
+    func(0);
 }

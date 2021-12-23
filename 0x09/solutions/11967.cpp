c3c1c98cc88c1b401fab54cc6274555c9c16c08d
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-12-23 16:59:58 +0900

    Update 11967.cpp

--- 0x09/solutions/11967.cpp
+++ 0x09/solutions/11967.cpp
@@ -6,15 +6,15 @@ using namespace std;
 #define X first
 #define Y second
 
-int board[101][101];
-bool vis[101][101];
+int board[101][101]; // (i, j)가 켜져있는가?
+bool vis[101][101]; // (i, j)를 방문했는가?
 vector<pair<int, int>> adj[101][101];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int n, m;
 bool OOB(int a, int b) { return a < 1 || a > n || b < 1 || b > n; }
 
-bool is_conn(pair<int, int> nxt) {
+bool is_conn(pair<int, int> nxt) { // (1,1)에서 nxt가 도달 가능한 칸인가?
   for (int dir = 0; dir < 4; dir++) {
     int nx = nxt.X + dx[dir];
     int ny = nxt.Y + dy[dir];
@@ -61,3 +61,8 @@ int main(void) {
     for (int j = 1; j <= n; j++) ans += board[i][j];
   cout << ans;
 }
+
+/*
+(아직 배우지 않은 내용이지만) 마치 인접 리스트 방식과 같이 연결 상태를 표현하는게 좋고(adj 배열)
+새롭게 불을 밝힌 방에 대해서 해당 방이 방문 가능한 곳인 경우에만 큐에 넣어주어야 한다.
+*/

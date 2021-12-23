1f812bc563daab29d34876b22acc3a8dd39b4a71
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2021-12-23 16:36:25 +0900

    Update 1759.cpp

--- 0x0C/solutions/1759.cpp
+++ 0x0C/solutions/1759.cpp
@@ -1,26 +1,25 @@
 // Authored by : yongjunleeme
 // Co-authored by : -
-// http://boj.kr/86e254ac9f8445f4b4aecc027b444f5b
+// http://boj.kr/04b7346a15bd441fb7c815c008682893
 #include <bits/stdc++.h>
 using namespace std;
 
 int l, c;
-unsigned char arr[17];
-unsigned char s[17];
+int arr[17];
+char s[17];
 bool isused[17];
+
+bool aeiou(char t){
+  return t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u';
+}
 
 void func(int k, int st){
   if(k == l){
     bool flag = false;
     int cnt1 = 0;
     int cnt2 = 0;
-
     for(int i = 0; i < l; i++){
-      if(s[arr[i]] == 'a' ||
-         s[arr[i]] == 'e' ||
-         s[arr[i]] == 'i' ||
-         s[arr[i]] == 'o' ||
-         s[arr[i]] == 'u') cnt1++;
+      if(aeiou(s[arr[i]])) cnt1++;
       else cnt2++;
     }
     if(cnt1 >= 1 && cnt2 >= 2) flag = true;

b99857ff0d9ba69d62422e77ad82d1a22135c7a1
hwang h <hehehwang@gmail.com>
2021-12-30 07:40:35 +0900

    Update 2750.cpp

--- 0x0E/solutions/2750.cpp
+++ 0x0E/solutions/2750.cpp
@@ -1,11 +1,17 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : heheHwang
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/1d051b9816e244069a1a73879cc0ef92
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  int N;
+  cin >> N;
+  vector<int> arr(N);
+  while (N--) cin >> arr[N];
+  sort(arr.begin(), arr.end());
+  for (int v : arr) cout << v << "\n";
 }

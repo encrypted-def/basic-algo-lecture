baf2e169b76655142e3d86d34e4a84372c79fd9e
hwang h <hehehwang@gmail.com>
2021-12-30 07:53:37 +0900

    Update 11931.cpp

--- 0x0E/solutions/11931.cpp
+++ 0x0E/solutions/11931.cpp
@@ -1,11 +1,21 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : heheHwang
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/59807f1dc1524512a0840b4ebb8998c9
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+const int MXN = 2'000'000, HALF = MXN / 2;
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  vector<bool> isnum(MXN + 2);
+  int N, t;
+  cin >> N;
+  while (N--) {
+    cin >> t;
+    isnum[t + HALF] = true;
+  }
+  for (int i = MXN; 0 <= i; i--)
+    if (isnum[i]) cout << i - HALF << '\n';
 }

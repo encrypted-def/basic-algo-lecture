deac104d36c17c557f575c7e5aff3e9bb6038c7a
heheHwang <hehehwang@gmail.com>
2022-01-11 19:39:02 +0900

    Update 13144.cpp

--- 0x14/solutions/13144.cpp
+++ 0x14/solutions/13144.cpp
@@ -1,11 +1,32 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : heheHwang
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/d810e653a6c84f6c881833ce759c4b0d
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  int N;
+  cin >> N;
+  vector<int> arr(N);
+  vector<bool> chk(100'002);
+  for (int i = 0; i < N; i++) cin >> arr[i];
+
+  int st = 0, en = 1;
+  long long ans = 0;
+  chk[arr[st]] = 1;
+  while (en != N) {
+    while (chk[arr[en]] && st != en) {
+      ans += (en - st);
+      chk[arr[st++]] = false;
+    }
+    chk[arr[en++]] = true;
+  }
+  // 등차수열의 합으로 처리해도 무방합니다.
+  while (st != N)
+    ans += (en - st++);
+
+  cout << ans;
 }

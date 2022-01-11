74d75af0f93be9f5a535f739054d1da93e3f36f3
heheHwang <hehehwang@gmail.com>
2022-01-11 19:31:30 +0900

    Update 22862.cpp

--- 0x14/solutions/22862.cpp
+++ 0x14/solutions/22862.cpp
@@ -1,11 +1,28 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : heheHwang
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/99ae176e3df145bb860d092f53e4c2c4
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int N, K, st, en, cnt, ans;
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> N >> K;
+  vector<int> arr(N);
+  for (int i = 0; i < N; i++)
+    cin >> arr[i];
+
+  for (; en < N; en++) {
+    cnt += arr[en] % 2;
+    /* 
+    지금 삭제한 홀수들이 K보다 많으면
+    st를 앞으로 옮기며 삭제합니다.
+    */
+    while (K < cnt)
+      cnt -= arr[st++] % 2;
+    ans = max(ans, en - st - cnt + 1);
+  }
+  cout << ans;
 }

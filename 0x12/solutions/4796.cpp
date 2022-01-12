6c42e2f043dad0879fa3ce6784b8b364de67f71c
heheHwang <hehehwang@gmail.com>
2022-01-12 09:14:46 +0900

    Update 4796.cpp

--- 0x12/solutions/4796.cpp
+++ 0x12/solutions/4796.cpp
@@ -1,11 +1,18 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : heheHwang
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/315381ca33a546b3b328747e31c36db9
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  int i = 1;
+  while (1) {
+    int L, P, V;
+    cin >> L >> P >> V;
+    if (!(L + P + V)) break;
+    cout << "Case " << i++ << ": " << V / P * L + min(V % P, L) << '\n';
+  }
 }

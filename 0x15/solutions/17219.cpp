58c8ae2f9fab63089b40da2f6a5258548c048113
gunny6026 <gunny6026@naver.com>
2022-01-12 19:37:36 +0900

    [0x15 해시] -  update 17219.cpp

--- 0x15/solutions/17219.cpp
+++ 0x15/solutions/17219.cpp
@@ -4,8 +4,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n,m;
+    cin >> n >> m;
+    unordered_map<string , string> hash;
+    string addr, password;
+    while (n--) {
+        cin >> addr >> password;
+        hash[addr] = password;
+    }
+    while (m --) {
+        cin >> addr;
+        cout << hash[addr] << '\n';
+    }
 }

11c0323ff13d03a20502e6f4d8e0780cc20d3a71
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-01-16 15:10:05 +0900

    Update 16933.cpp

--- 0x09/solutions/16933.cpp
+++ 0x09/solutions/16933.cpp
@@ -1,55 +1,68 @@
 // Authored by : heheHwang
-// Co-authored by : -
-// http://boj.kr/d7122ff6c350443aaafda72f0d8d8d75
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/77063fc182b64ce689f3636e86bd2eef
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m, k;
 string board[1002];
-int N, M, K;
-bool vis[1002][1002][12];
+// x, y, 부순 횟수, 낮/밤 여부(낮 = 0, 밤 = 1)
+int dist[1002][1002][12][2];
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> N >> M >> K;
-  for (int i = 0; i < N; i++)
+  cin >> n >> m >> k;
+  for (int i = 0; i < n; i++)
     cin >> board[i];
 
-  // [x좌표, y좌표, 거리, 부순 횟수]로 큐를 구성합니다.
   queue<tuple<int, int, int, int>> q;
-  q.push({0, 0, 1, 0});
-  vis[0][0][0] = true;
-  while (q.size()) {
-    auto [x, y, d, b] = q.front();
+  q.push({0, 0, 0, 0});
+  dist[0][0][0][0] = 1;
+  while (!q.empty()) {
+    auto [x, y, w, t] = q.front();
     q.pop();
-    if (x == N - 1 && y == M - 1) {
-      cout << d;
+    if (x == n - 1 && y == m - 1) {
+      cout << dist[x][y][w][t] << '\n';
       return 0;
     }
-    bool wait = false;
-    for (int k = 0; k < 4; k++) {
-      int nx = x + "1120"[k] - '1', ny = y + "2011"[k] - '1';
-      if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+    for (int dir = 0; dir < 4; dir++) {
+      int nx = x + dx[dir];
+      int ny = y + dy[dir];
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m)
         continue;
-      if (board[nx][ny] == '0') {
-        if (vis[nx][ny][b]) continue;
-        vis[nx][ny][b] = true;
-        q.push({nx, ny, d + 1, b});
-      } else if (b < K) {
-        /*
-        만약 부술 여유가 남아있고, 현재 낮이라면 당장 부수고,
-        밤이라면 일단 큐에 밀어넣고 다음날을 기다립니다.
-        */
-        if (d % 2) {
-          if (vis[nx][ny][b + 1]) continue;
-          vis[nx][ny][b + 1] = true;
-          q.push({nx, ny, d + 1, b + 1});
-        } else
-          wait = true;
+      // 만약 (nx, ny)가 빈 곳이라면 편하게 가면 된다.
+      if(board[nx][ny] == '0'){
+        int nw = w;
+        int nt = 1-t;
+        if(dist[nx][ny][nw][nt] > 0) continue;
+        dist[nx][ny][nw][nt] = dist[x][y][w][t] + 1;
+        q.push({nx, ny, nw, nt});
+      }
+      // 비어있지 않다면
+      else{
+        // 이미 k번 깼으면 더 깰 수 없다.
+        if(w == k) continue;
+        // 낮이라면 깨고 넘어가면 된다.
+        if(t == 0){
+          int nw = w+1;
+          int nt = 1-t;
+          if(dist[nx][ny][nw][nt] > 0) continue;
+          dist[nx][ny][nw][nt] = dist[x][y][w][t] + 1;
+          q.push({nx, ny, nw, nt});
+        }
+        // 밤이라면 낮이 올 때 까지 기다려야 한다.
+        // 다음 좌표가 (nx, ny, nw, nt)가 아닌 (x, y, w, nt)라는 점에 주의한다.
+        else{
+          int nt = 1-t;
+          if(dist[x][y][w][nt] > 0) continue;
+          dist[x][y][w][nt] = dist[x][y][w][t] + 1;
+          q.push({x, y, w, nt});
+        }
       }
     }
-    if (wait)
-      q.push({x, y, d + 1, b});
   }
   cout << -1;
 }

eb620c2174f739bb03c0b03f7555bc3ba1109623
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-01-16 14:24:05 +0900

    Update 17070.cpp

--- 0x0D/solutions/17070.cpp
+++ 0x0D/solutions/17070.cpp
@@ -1,51 +1,39 @@
 // Authored by : sukam09
-// Co-authored by : -
-// http://boj.kr/e4a058bedcd44f5d945246a0fd5dfdaf
-
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/953959bfe6ae4931a1e1961286815365
 #include <bits/stdc++.h>
 using namespace std;
 
 int n;
 int board[21][21];
 int ans;
+int dx[3] = {0, 1, 1};
+int dy[3] = {1, 1, 0};
 
 // 범위 밖을 벗어났는지 체크하는 함수
 bool oob(int x, int y) {
   return x < 0 || x >= n || y < 0 || y >= n;
 }
 
-// 밀어서 가로 방향의 파이프로 바꿀 수 있는지 체크하는 함수
-bool chkh(int x, int y) {
-  return !(oob(x, y + 1) || board[x][y + 1]);
-}
-
-// 밀어서 세로 방향의 파이프로 바꿀 수 있는지 체크하는 함수
-bool chkv(int x, int y) {
-  return !(oob(x + 1, y) || board[x + 1][y]);
-}
-
-// 밀어서 대각선 아래 방향의 파이프로 바꿀 수 있는지 체크하는 함수
-bool chkd(int x, int y) {
-  return !(oob(x + 1, y + 1) || board[x][y + 1] || board[x + 1][y] || board[x + 1][y + 1]);
-}
-
-void sim(int x, int y, int dir) {
+void sim(int x, int y, int before_dir) {
   if (x == n - 1 && y == n - 1) {
     ans++;
     return;
   }
 
-  // dir이 0, 1, 2일 때 각각 현재 파이프의 방향이 가로, 세로, 대각선 아래를 나타냄
-  if (dir == 0) {
-    if (chkh(x, y)) sim(x, y + 1, 0);
-    if (chkd(x, y)) sim(x + 1, y + 1, 2);
-  } else if (dir == 1) {
-    if (chkv(x, y)) sim(x + 1, y, 1);
-    if (chkd(x, y)) sim(x + 1, y + 1, 2);
-  } else {
-    if (chkh(x, y)) sim(x, y + 1, 0);
-    if (chkv(x, y)) sim(x + 1, y, 1);
-    if (chkd(x, y)) sim(x + 1, y + 1, 2);
+  for(int dir = 0; dir < 3; dir++){
+    // 45도 제한 조건
+    if(before_dir == 0 && dir == 2) continue;
+    if(before_dir == 2 && dir == 0) continue;
+    
+    int nx = x + dx[dir];
+    int ny = y + dy[dir];
+    if(oob(nx, ny) || board[nx][ny]) continue;
+    // 대각선으로 이동할 때에 한해 2칸을 더 고려해야 함
+    if(dir == 1){
+      if(board[nx][y] || board[x][ny]) continue;
+    }
+    sim(nx, ny, dir);
   }
 }
 
@@ -57,12 +45,7 @@ int main(){
   for (int i = 0; i < n; i++)
     for(int j = 0; j < n; j++)
       cin >> board[i][j];
-  ans = 0;
-  
-  // (N, N)에 벽이 있으면 이동시킬 수 없음
-  if (board[n - 1][n - 1]) cout << 0;
-  else {
-    sim(0, 1, 0);
-    cout << ans;
-  }
+
+  sim(0, 1, 0);
+  cout << ans;
 }

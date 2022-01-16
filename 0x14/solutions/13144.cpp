110f6bca267a2c808c39f169de329cfe58b7e97a
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-01-16 15:22:08 +0900

    Update 13144.cpp

--- 0x14/solutions/13144.cpp
+++ 0x14/solutions/13144.cpp
@@ -1,6 +1,6 @@
 // Authored by : heheHwang
-// Co-authored by : -
-// http://boj.kr/d810e653a6c84f6c881833ce759c4b0d
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/20b3510ea5384c78a6e1c34aae40b267
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,25 +8,23 @@ int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int N;
-  cin >> N;
-  vector<int> arr(N);
+  int n;
+  cin >> n;
+  vector<int> arr(n);
   vector<bool> chk(100'002);
-  for (int i = 0; i < N; i++) cin >> arr[i];
+  for (int i = 0; i < n; i++) cin >> arr[i];
 
-  int st = 0, en = 1;
   long long ans = 0;
-  chk[arr[st]] = 1;
-  while (en != N) {
-    while (chk[arr[en]] && st != en) {
-      ans += (en - st);
-      chk[arr[st++]] = false;
+  chk[arr[0]] = 1;
+  int en = 0;
+  for(int st = 0; st < n; st++){
+    // en에는 arr[st] to arr[en]에 같은 수가 등장하지 않는 가장 큰 값이 저장됨
+    while (en < n-1 && !chk[arr[en+1]]){
+      en++;
+      chk[arr[en]] = 1;
     }
-    chk[arr[en++]] = true;
+    ans += (en-st+1);
+    chk[arr[st]] = 0;
   }
-  // 등차수열의 합으로 처리해도 무방합니다.
-  while (st != N)
-    ans += (en - st++);
-
   cout << ans;
 }

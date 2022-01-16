004c23c877f41b221ad0ecdaabde2c9036a08a37
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-01-16 14:46:46 +0900

    Update 14442.cpp

--- 0x09/solutions/14442.cpp
+++ 0x09/solutions/14442.cpp
@@ -1,44 +1,51 @@
 // Authored by : heheHwang
-// Co-authored by : -
-// http://boj.kr/ef352cb178c94bb4afe1f82079ceab65
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/2603556b095b406c924707a1901cc884
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, K;
+int n, m, k;
 string board[1002];
 // x, y, 부순 횟수
-bool vis[1002][1002][12];
+int dist[1002][1002][12];
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> N >> M >> K;
-  for (int i = 0; i < N; i++)
+  cin >> n >> m >> k;
+  for (int i = 0; i < n; i++)
     cin >> board[i];
 
-  // [x좌표, y좌표, 거리, 부순 횟수]로 큐를 구성한다.
-  queue<tuple<int, int, int, int>> q;
-  q.push({0, 0, 0, 0});
-  vis[0][0][0] = true;
-  while (q.size()) {
-    auto [x, y, d, b] = q.front();
+  // [x좌표, y좌표, 부순 횟수]로 큐를 구성한다.
+  queue<tuple<int, int, int>> q;
+  q.push({0, 0, 0});
+  dist[0][0][0] = 1;
+  while (!q.empty()) {
+    auto [x, y, w] = q.front();
     q.pop();
-    if (x == N - 1 && y == M - 1) {
-      cout << d + 1 << '\n';
+    if (x == n - 1 && y == m - 1) {
+      cout << dist[x][y][w] << '\n';
       return 0;
     }
-    vis[x][y][b] = true;
-    for (int k = 0; k < 4; k++) {
-      int nx = x + "1120"[k] - '1', ny = y + "2011"[k] - '1';
-      if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+    for (int dir = 0; dir < 4; dir++) {
+      int nx = x + dx[dir];
+      int ny = y + dy[dir];
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m)
         continue;
       // 만약 여기가 블록이라면 부순 횟수를 늘린다.
-      int nb = b + (board[nx][ny] == '1');
-      if (K < nb) continue;
-      if (vis[nx][ny][nb]) continue;
-      vis[nx][ny][nb] = true;
-      q.push({nx, ny, d + 1, nb});
+      int nw = w;
+      if(board[nx][ny] == '1') nw++;
+      if (nw > k || dist[nx][ny][nw] > 0) continue;
+      dist[nx][ny][nw] = dist[x][y][w] + 1;
+      q.push({nx, ny, nw});
     }
   }
   cout << -1;
 }
+/*
+상태 배열에 벽을 부순 횟수까지 포함시켜야 한다.
+이 문제에서는 dist의 시작값이 0이 아니라 1이어서 초기값을 -1로
+초기화시킬 필요가 없었다.
+*/

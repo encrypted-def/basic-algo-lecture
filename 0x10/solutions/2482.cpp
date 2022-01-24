d4d696a10db9a1c0dc6e5977aac544a65811fb09
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-01-24 10:39:47 +0900

    Update 2482.cpp

--- 0x10/solutions/2482.cpp
+++ 0x10/solutions/2482.cpp
@@ -1,10 +1,12 @@
 // Authored by : jihwan0123
-// Co-authored by : -
-// http://boj.kr/17eeefb8dd884149b200ec7b6a3a944e
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/f238e914a2b2492ba3bcff9f82879d63
 #include <bits/stdc++.h>
 using namespace std;
+
 int d[1005][1005]; // d[i][j] : i개의 색 중 j개의 색 선택하는 경우의 수
 const int mod = 1e9 + 3;
+
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -13,7 +15,7 @@ int main(void) {
   for (int i = 1; i <= n; i++) d[i][1] = i; // i개중 1개 고르는 방법: i개
   for (int i = 4; i <= n; i++)
     for (int j = 2; j <= k; j++) {
-      if (j > i / 2) break; // n개 중에서 2/n개 이상 선택 불가
+      if (j > i / 2) break; // n개 중에서 n/2개 초과로 선택 불가
       // i-1개중 j개 선택 + n번째 색 선택 x
       // i-2개중 j-1개 선택 + n번째 색 선택 o
       d[i][j] = (d[i - 1][j] + d[i - 2][j - 1]) % mod;

2064917c88319fb923ea4f2b4444d70db8bf4ca0
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-01-30 23:28:44 +0900

    Update 17142.cpp

--- 0x0D/solutions/17142.cpp
+++ 0x0D/solutions/17142.cpp
@@ -1,9 +1,6 @@
 // Authored by : yongjunleeme
-// Co-authored by : -
-// http://boj.kr/30955cc755a74a65b7126ac824054d40
-#include <bits/stdc++.h>
-using namespace std;
-
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/c0eb9e2e9bbe4ebeb386e2635ba01a68
 #include <bits/stdc++.h>
 using namespace std;
 #define X first
@@ -14,13 +11,42 @@ int n, m;
 int board[55][55];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-vector<pair<int, int>> v;
+vector<pair<int, int>> place; // 바이러스가 있을 수 있는 칸
 queue<pair<int, int>> q;
 int ans = MX;
 int dist[55][55];
-int vis[55][55];
-int emptyCnt = 0;
-bool flag = false;
+int emptyCnt = 0; // 빈 칸의 수
+vector<int> brute; // next_permutation을 돌리기 위한 배열
+int bfs(){
+  for(int i = 0; i < n; i++) fill(dist[i], dist[i]+n, -1);
+
+  for(int i = 0; i < place.size(); i++){
+    if(brute[i]) continue;
+    q.push({place[i].X, place[i].Y});
+    dist[place[i].X][place[i].Y] = 0;
+  }
+
+  int num = 0;
+  int times = 0;
+  while(!q.empty()){
+    auto cur = q.front(); q.pop();
+    for(int dir = 0; dir < 4; dir++){
+      int nx = cur.X + dx[dir];
+      int ny = cur.Y + dy[dir];
+      if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+      if(board[nx][ny] == 1 || dist[nx][ny] >= 0) continue;
+      q.push({nx, ny});
+      dist[nx][ny] = dist[cur.X][cur.Y]+1;
+
+      if(board[nx][ny] != 2){
+        times = dist[nx][ny];
+        num++;
+      }
+    }
+  }
+  if(emptyCnt == num) return times;
+  return MX;
+}
 
 int main(void){
   ios::sync_with_stdio(0);
@@ -30,49 +56,14 @@ int main(void){
     for(int j = 0; j < n; j++){
       cin >> board[i][j];
       if(board[i][j] == 0) emptyCnt++;
-      if(board[i][j] == 2) v.push_back({i, j});
+      if(board[i][j] == 2) place.push_back({i, j});
     }
   }
-  if(!emptyCnt){
-    cout << 0;
-    return 0;
-  }
-
-  vector<int> brute(v.size());
-  for(int i = m; i < v.size(); i++) brute[i] = 1;
-
+  brute.resize(place.size());
+  fill(brute.begin() + m, brute.end(), 1);
   do{
-    for(int i = 0; i < n; i++) fill(dist[i], dist[i]+n, -1);
-
-    for(int i = 0; i < v.size(); i++){
-      if(!brute[i]){
-        q.push({v[i].X, v[i].Y});
-        dist[v[i].X][v[i].Y] = 0;
-      }
-    }
-
-    int emptyCnt2 = 0;
-    int virusCnt = 0;
-    while(!q.empty()){
-      auto cur = q.front(); q.pop();
-      for(int dir = 0; dir < 4; dir++){
-        int nx = cur.X + dx[dir];
-        int ny = cur.Y + dy[dir];
-        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-        if(board[nx][ny] == 1 || dist[nx][ny] >= 0) continue;
-        q.push({nx, ny});
-        dist[nx][ny] = dist[cur.X][cur.Y]+1;
-
-        if(board[nx][ny] != 2){
-          virusCnt = max(virusCnt, dist[nx][ny]);
-          emptyCnt2++;
-        }
-      }
-    }
-    if(emptyCnt == emptyCnt2)
-      ans = min(ans, virusCnt);
+    ans = min(ans, bfs());
   }while(next_permutation(brute.begin(), brute.end()));
-
   if(ans == MX) cout << -1;
   else cout << ans;
 }

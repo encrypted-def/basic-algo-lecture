9266bc416e846edd0042b6d03c3278c66496bc36
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-02-09 22:06:21 +0900

    Update 14889.cpp

--- 0x0D/solutions/14889.cpp
+++ 0x0D/solutions/14889.cpp
@@ -1,34 +1,11 @@
 // Authored by : sukam09
-// Co-authored by : -
-// http://boj.kr/906682652b9a4e9d9ecc83a38e84936b
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/a75486aadfc04beeb40786a0728ffc78
 #include <bits/stdc++.h>
 using namespace std;
 
 int n;
 int a[25][25];
-int cand[25];
-vector<int> p, s, l;
-int mx = 0x7f7f7f7f;
-
-void getp() {
-  int tmp[n / 2];
-  fill(tmp, tmp + n / 2, 1);
-  tmp[0] = 0, tmp[1] = 0;
-  do {
-    for (int i = 0; i < n / 2; i++)
-      if (tmp[i] == 0) p.push_back(i);
-  } while (next_permutation(tmp, tmp + n / 2));
-}
-
-int solve() {
-  int ss = 0, ls = 0;
-  for (int i = 0; i < p.size(); i += 2) {
-    int x = p[i], y = p[i + 1];
-    ss += a[s[x]][s[y]] + a[s[y]][s[x]];
-    ls += a[l[x]][l[y]] + a[l[y]][l[x]];
-  }
-  return abs(ss - ls);
-}
 
 int main(void) {
   ios::sync_with_stdio(0);
@@ -36,26 +13,22 @@ int main(void) {
 
   cin >> n;
   for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++)
-      cin >> a[i][j];
+    for (int j = 0; j < n; j++) cin >> a[i][j];
 
-  getp();
-  for (int i = n / 2; i < n; i++) cand[i] = 1;
-  int ans = mx;
-  
+  vector<int> team(n);
+  fill(team.begin() + n / 2, team.end(), 1);
+  int ans = 0x7f7f7f7f;
+  // team = {0, 0, 0, .., 0, 1, 1, .. ,1}, team[i]는 i번째 선수의 팀을 의미
   do {
-    s.clear(); l.clear();
+    int diff = 0; // 능력치의 차이
     for (int i = 0; i < n; i++) {
-      if (cand[i] == 0) s.push_back(i);
-      else l.push_back(i);
+      for (int j = i+1; j < n; j++){
+        if(team[i] != team[j]) continue;
+        if(team[i] == 0) diff += (a[i][j] + a[j][i]);
+        else diff -= (a[i][j] + a[j][i]);
+      }
     }
-    int res = solve();
-    ans = min(ans, res);
-  } while (next_permutation(cand, cand + n));
-  
+    ans = min(ans, abs(diff));    
+  } while (next_permutation(team.begin(), team.end()));
   cout << ans;
 }
-/*
-0 ~ n / 2 - 1까지의 숫자 중에서 짝을 지을 수 있는 모든 경우를 getp()를 통해 미리 벡터에 2개씩 저장해 둔다.
-이를 앞에서부터 2개씩 가져와서 각각 s와 l에 인덱스로 대입하여 실제 팀원 번호를 구한다.
-*/

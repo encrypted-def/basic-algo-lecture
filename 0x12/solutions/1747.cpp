84cbd459be97ceb1286e7006b1b3c6ccc1a416f6
sukam09 <sukam09@naver.com>
2022-02-15 15:35:39 +0900

    Update 1747.cpp

--- 0x12/solutions/1747.cpp
+++ 0x12/solutions/1747.cpp
@@ -1,11 +1,59 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : sukam09
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/00547c6365314010a6b896072a7d4ea5
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int n;
+vector<bool> p(10000000, true); // 소수 판정
+vector<bool> pl(100000000); // 팰린드롬 판정
+int p10[5] = {1, 10, 100, 1000, 10000};
+
+void sieve() {
+  p[0] = p[1] = false;
+  for (int i = 2; i * i <= 9999999; i++) {
+    if (!p[i]) continue;
+    for (int j = i * i; j <= 9999999; j += i)
+      p[j] = false;
+  }
+}
+
+int reverse(int n) {
+  int ret = 0;
+  while (n) {
+    ret = ret * 10 + n % 10;
+    n /= 10;
+  }
+  return ret;
+}
+
+void palindrome() {
+  for (int i = 1; i <= 9999; i++) {
+    int sz = to_string(i).size();
+    int lo = p10[sz - 1];
+    int hi = p10[sz];
+    
+    int rev = reverse(i);
+    int tail = rev % lo; // n을 뒤집은 수에서 가장 앞에 있는 숫자를 제거한 수
+    
+    pl[i * lo + tail] = true;
+    pl[i * hi + rev] = true;
+  }
+}
+
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> n;
+  sieve(); palindrome();
+  for (int i = n; i <= 9999999; i++) {
+    if (p[i] && pl[i]) {
+      cout << i;
+      return 0;
+    }
+  }
 }
+/*
+palindrome 함수에서 i = 1234일 때 (i * lo + tail) = 1234321, (i * hi + rev) = 12344321이 된다.
+*/

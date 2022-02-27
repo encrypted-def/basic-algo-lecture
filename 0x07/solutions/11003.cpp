67ed196f465f01d57115fb70a6d9d8a2315498e2
unluckyjung <ybook2012@gmail.com>
2022-02-27 14:49:32 +0900

    Update: 11003.cpp

--- 0x07/solutions/11003.cpp
+++ 0x07/solutions/11003.cpp
@@ -1,11 +1,38 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : unluckyjung
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/357fbc7fbbee4a08bc35d9806233f6ac
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  int n, m;
+  deque<pair<int, int>> dq;
+
+  cin >> n >> m;
+
+  for (int curIndex = 0; curIndex < n; ++curIndex) {
+    int num;
+    cin >> num;
+
+    // 새로운 숫자가 deque내에서 가장 큰 값의 숫자이고 마지막에 위치하도록 합니다.
+    // deque안의의 숫자들은 오름차순 상태를 유지하게 되고, 구간내 가장 작은값은 항상 맨앞(front)에 있는 상태를 가지게 됩니다.
+    while (!dq.empty() && dq.back().second >= num) {
+      dq.pop_back();
+    }
+    
+    // 새로운 숫자는 항상 삽입됩니다.
+    dq.push_back({curIndex, num});
+
+    // 주어진 구간을 벗어나는 outdate된 숫자들은 제거해줍니다.
+    if (dq.front().first <= curIndex - m) {
+      dq.pop_front();
+    }
+
+    cout << dq.front().second << " ";
+  }
+
+  return 0;
 }

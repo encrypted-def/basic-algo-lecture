ecf2fc8029d4d6c5ea9fd30f4263a417b55251d3
SciEm <yhj990528@gmail.com>
2022-03-23 14:46:28 +0900

    Update 1193.cpp

--- 0x12/solutions/1193.cpp
+++ 0x12/solutions/1193.cpp
@@ -1,11 +1,20 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : SciEm
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/037f3a0f8454498d8b609d819fd67398
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  int x; cin >> x;
+
+  int i = 1;
+  while (x > i) x -= i++;
+
+  int nume = x;
+  int deno = i + 1 - x;
+  if (i % 2) swap(nume, deno);
+  cout << nume << '/' << deno;
 }

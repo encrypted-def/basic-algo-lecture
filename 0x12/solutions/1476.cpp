38f2f26d24a3b44055137e48b44d44b5ebee08a1
SciEm <yhj990528@gmail.com>
2022-03-25 17:22:58 +0900

    Update 1476.cpp

--- 0x12/solutions/1476.cpp
+++ 0x12/solutions/1476.cpp
@@ -1,11 +1,22 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : SciEm
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/f10dafa7b28241d7a173b2be389804bf
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int e, s, m;
+
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> e >> s >> m;
+  e--, s--, m--;
+
+  int i = e;
+  for (; i % 28 != s; i += 15);
+
+  int j = i;
+  for (; j % 19 != m; j += lcm(15, 28));
+  cout << j + 1;
 }

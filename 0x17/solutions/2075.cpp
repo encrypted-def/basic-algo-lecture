d77b55331b7f8d6f4419a6b0570548aec77bfabc
heheHwang <hehehwang@gmail.com>
2022-03-31 11:36:43 +0900

    Update 2075.cpp

--- 0x17/solutions/2075.cpp
+++ 0x17/solutions/2075.cpp
@@ -1,11 +1,20 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : heheHwang
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/45e1c40b488544b89ca5d9c966a6cdc0
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  int N, num;
+  priority_queue<int, vector<int>, greater<>> pq;
+  cin >> N;
+  for (int i = 0; i < N * N; i++) {
+    cin >> num;
+    pq.push(num);
+    if (N < (int)pq.size()) pq.pop();
+  }
+  cout << pq.top() << '\n';
 }

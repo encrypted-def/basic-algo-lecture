1875fd42e452caae82064b5c7f9b434c11c3d2a2
ximgea <ximgea@gmail.com>
2022-04-12 16:01:11 +0900

    0x15 1351

--- 0x15/solutions/1351.cpp
+++ 0x15/solutions/1351.cpp
@@ -1,11 +1,29 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : ximgea
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/d77ea5b806134b5898973f87ece76dae
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
+long long n,p,q;
+unordered_map<long long,long long> table;
+
+long long f(long long i) {
+  if(table.find(i) != table.end())
+    return table[i];
+
+  if (i == 0) {
+    table[i] = 1;
+    return table[i];
+  }
+
+  table[i] = f(i/p) + f(i/q);
+  return table[i];
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> n >> p >> q;
+  cout << f(n);
 }

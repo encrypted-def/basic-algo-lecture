7308a20fc15bc293d7dc4de40a0a2cbb32965234
ximgea <ximgea@gmail.com>
2022-04-12 16:55:47 +0900

    0x18 1325

--- 0x18/solutions/1325.cpp
+++ 0x18/solutions/1325.cpp
@@ -1,11 +1,43 @@
 // Authored by : BaaaaaaaaaaarkingDog
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/7fcf2dccf277465db272d434f1f98b1a
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
+vector<int> adj[10'005];
+bool vis[10'005];
+int hack_cnt[10'005];
+
+int dfs(int u) {
+  int cnt = 1;
+  vis[u] = true;
+  for(auto v : adj[u]) {
+    if(vis[v]) continue;
+    cnt += dfs(v);
+  }
+  return cnt;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n,m;
+  cin >> n >> m;
+  for(int i{}; i<m; ++i) {
+    int u, v;
+    cin >> v >> u;
+    adj[u].push_back(v);
+  }
+
+  for(int i{1}; i<=n; ++i) {
+    fill(vis,vis+n+1,false);
+    hack_cnt[i] = dfs(i);
+  }
+
+  auto max_cnt = *max_element(hack_cnt,hack_cnt+n+1);
+
+  for(int i{1}; i<=n; ++i)
+    if(hack_cnt[i] == max_cnt)
+      cout << i << ' ';
 }

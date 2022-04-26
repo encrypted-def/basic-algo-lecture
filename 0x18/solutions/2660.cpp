d1c3144cd23f0af4c379c2f6ee37f5e0c4bd9fc4
swlee <swo98@naver.com>
2022-04-27 02:01:06 +0900

    Update 2660.cpp

--- 0x18/solutions/2660.cpp
+++ 0x18/solutions/2660.cpp
@@ -15,36 +15,34 @@ bool vis[101];
 int n,m; // n:컴퓨터 수, m:네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수
 
 void bfs(){
-	queue<int> q;
-	q.push(1);
-	vis[1] = true;
+  queue<int> q;
+  q.push(1);
+  vis[1] = true;
 
-	int cnt = 0;
-	while(!q.empty()){
-		int cur = q.front(); q.pop();
-		for(auto nxt : adj[cur]){
-			if(vis[nxt]) continue;
-			q.push(nxt);
-			vis[nxt] = true;
-			cnt++;
-		}
-	}
-
-	cout<<cnt;
+  int cnt = 0;
+  while(!q.empty()){
+    int cur = q.front(); q.pop();
+    for(auto nxt : adj[cur]){
+      if(vis[nxt]) continue;
+      q.push(nxt);
+      vis[nxt] = true;
+      cnt++;
+    }
+  }
+  cout<<cnt;
 }
 
 int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-	cin>>n>>m;
-	while(m--){
-		int u,v;
-		cin >> u >> v;
+  cin>>n>>m;
+  while(m--){
+    int u,v;
+    cin >> u >> v;
 
-		adj[u].push_back(v);
-		adj[v].push_back(u);
-	}
-
-	bfs();
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+   }
+   bfs();
 }

1a1164419096354af075fef314d5ff6859baaf87
yongjunleeme <chesidyong@gmail.com>
2022-08-18 09:59:53 +0900

    (0x10 다이나믹 프로그래밍) Fix 9251.cpp
    - d 정의와 식의 이유 주석으로 추가

--- 0x10/solutions/9251.cpp
+++ 0x10/solutions/9251.cpp
@@ -1,10 +1,15 @@
 // Authored by : yongjunleeme
 // Co-authored by : -
-// http://boj.kr/73844bd37f6a4dbb9e036d53dab8ad65
+// http://boj.kr/33e1192d073d42e4853b36bc1cfd597d
 #include <bits/stdc++.h>
 using namespace std;
 
 int d[1005][1005];
+// d[i][j] = a의 i-1번째 글자와 b의 j-1번째 글자까지 최장 공통 부분수열
+// 최장 공통부분 수열을 보존하기 위해 2중 for문을 사용
+// d[i][j] --> if(a[i-1] == b[j-1])이면 d[i-1][j-1] + 1
+// d[i][j] --> if(a[i-1] != b[j-1])이면 max(d[i-1][j], d[i][j-1])
+// 초기값은 d[1][1]에 a[0]과 b[0]이 같을 때와 a[0]과 b[0]이 다를 때 각각 다른 값을 저장
 
 int main(void){
   ios::sync_with_stdio(0);

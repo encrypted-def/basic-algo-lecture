dcef9681d80b6cbfa39885b4aad14603f1500a78
neppiness <0414kjh@naver.com>
2022-09-05 16:22:10 +0900

    Update 1351.cpp of Ch.0x15

--- 0x15/solutions/1351.cpp
+++ 0x15/solutions/1351.cpp
@@ -1,11 +1,26 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : scsc3204
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/e81859b0268548ffad93a45b285dab49
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+ll n;
+int p, q;
+unordered_map<ll, ll> um;
+
+ll solve(ll x) {
+  if(x == 0) return 1;
+  if(um[x]) return um[x];
+
+  um[x] = solve(x/p) + solve(x/q);
+  return um[x];
+}
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> n >> p >> q;
+  cout << solve(n);
 }

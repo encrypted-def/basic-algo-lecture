65fbf22565aa648b79917db005da04063e7a1023
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-11-04 22:38:59 +0900

    Update 13335_1.cpp

--- 0x0D/solutions/13335_1.cpp
+++ 0x0D/solutions/13335_1.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int n, w, L;
-int a[1000];
-int cross[1000];
+int a[1000]; // i번째 트럭의 무게
+int cross[1000]; // cross[i] : i번째 트럭이 다리에 올라간 시간
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -14,7 +14,7 @@ int main(void) {
   for (int i = 0; i < n; i++) cin >> a[i];
   cross[0] = 1;
   int sum = a[0];
-  int idx = 0;
+  int idx = 0; // 현재 다리에 올라가있는 트럭 중 가장 앞에 있는 트럭의 번호
   for (int i = 1; i < n; i++) {
     while (i - idx + 1 > w || sum + a[i] > L) sum -= a[idx++];
     sum += a[i];

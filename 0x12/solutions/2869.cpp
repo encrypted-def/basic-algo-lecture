da70fc3b9653351397404589777738033640fcb3
MJ-SEO <audwns392@naver.com>
2022-11-14 18:24:02 +0900

    2869

--- 0x12/solutions/2869.cpp
+++ 0x12/solutions/2869.cpp
@@ -1,11 +1,19 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : audwns27
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/a9eb8b5b0eea4c5aaa2ffd3331ec2208
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   
+  int day, night, height;
+  cin >> day >> night >> height;
+
+  cout << (height - night -1) / (day-night) + 1;
+
+  return 0;
 }

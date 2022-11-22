e69c4cdcab0fd6c4abf4f266cec10c55d8fe69a8
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2022-11-22 13:09:26 +0900

    Update 2869.cpp

--- 0x12/solutions/2869.cpp
+++ 0x12/solutions/2869.cpp
@@ -1,35 +1,29 @@
 // Authored by : audwns27
-// Co-authored by : -
+// Co-authored by : BaaaaaaaaaaarkingDog
 // http://boj.kr/a9eb8b5b0eea4c5aaa2ffd3331ec2208
-
 #include <bits/stdc++.h>
-
 using namespace std;
-
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   
-  int day, night, height;
-  cin >> day >> night >> height;
-
-  cout << (height - night - 1) / (day-night) + 1;
-
-  return 0;
+  int a, b, v;
+  cin >> a >> b >> v;
+  if((v-b) % (a-b) == 0) cout << (v-b) / (a-b);
+  else cout << (v-b) / (a-b) + 1;
 }
 
 /*
+2 1 1000000000과 같은 데이터를 생각한다면 매일 매일의 움직임을 정직하게 따라가는 방식으로 구현할 경우 시간초과가 남을 알 수 있다.
+수학적으로 생각해보면 달팽이가 X일 동안 오른다고 하면 마지막 날은 A만큼, 나머지 날은 A-B만큼 오르기 때문에
+(A-B) * (X-1) + A >= V이고, 이를 만족하는 가장 작은 X가 곧 답이 된다.
 
-단순 반복문으로 작성시 O(N)으로 heigth < 1,000,000,000 범위에서 시간초과
-
-하루 동안 움직이는 거리 = (day - night)
-목표 높이 = height 
-목표 도달 당일에는 미끄러지지 않기 때문에 총 (height - night) 높이를 올라가야 한다.
-
-(day - night)미터씩 올라가 (heigth - nigth)미터와 같거나 작은 거리가 남는 날이 정답. 
-
-만일 (height - night)가 정확히 (day - night)로 나누어떨어지지 않는다면, 몫에 1을 더한 값이 정답.
-
-하지만 (height - night)가 정확히 (day - night)로 나누어떨어지는 경우를 감안해서 처음부터 (height - night)에 1을 더 빼고 나눈 후, 무조건 1을 더하는 방식 사용.
-
+식을 풀어보면
+(A-B) * (X-1) + A >= V
+(A-B) * X - (A-B) + A >= V
+(A-B) * X + B >= V
+(A-B) * X >= (V-B)
+X >= (V-B) / (A-B)
+가 되고, (V-B)가 (A-B)로 나누어 떨어진다면 (V-B) / (A-B),
+나누어 떨어지지 않는다면 (V-B) / (A-B) + 1이 답이 된다.
 */

094b22a04c5fd402e090e0a0809655f985eb6fea
jglee <jinkyu0826@gmail.com>
2023-01-12 14:06:47 +0900

    Update 13141.cpp
    coding convention에 맞춰 수정

--- 0x1C/solutions/13141.cpp
+++ 0x1C/solutions/13141.cpp
@@ -1,8 +1,7 @@
 // Authored by : jkl
 // Co-authored by : -
-// http://boj.kr/f4b4371b42204ad3984e85124a268465
+// http://boj.kr/9109a26601fc48c08c732e3daf5be675
 #include <bits/stdc++.h>
-#define INF 0x3f3f3f3f
 using namespace std;
 int main() {
   ios::sync_with_stdio(0);
@@ -10,7 +9,7 @@ int main() {
 
   int n, m;
   cin >> n >> m;
-  vector<vector<int>> floyd(n + 1, vector<int>(n + 1, INF));
+  vector<vector<int>> floyd(n + 1, vector<int>(n + 1, 0x3f3f3f3f));
   vector<tuple<int, int, int>> edges;
   for (int i = 1; i <= n; i++)
     floyd[i][i] = 0;
@@ -29,7 +28,7 @@ int main() {
     }
   }
 
-  double ans = INF;
+  double ans = 0x3f3f3f3f;
   for (int i = 1; i <= n; i++) {
     double totalTime = 0;
     for (auto [s, e, l] : edges) {
@@ -47,8 +46,8 @@ int main() {
   2. X -> B 정점으로 불이 이동하는 최단시간
   3. A-B 간선의 길이
 
-  (1+2+3)/2 로 해당 간선이 불에 타는 최소시간을 구할 수 있다. (이해가 안갈 경우 X-A-B를 연결한 고무줄을 반으로 자른다고 생각해보자.)
-  모든 간선이 불에 타는시간은 가장 긴 간선이 불에 타는 시간과 동일하다.
+  (1+2+3)/2 로 해당 간선이 불에 타는 최소시간을 구할 수 있다. (이해가 안갈 경우 X-A-B를 연결한 고무줄을 반으로 자른다고 생각해보자.) 모든 간선이 불에 타는
+  시간은 가장 긴 간선이 불에 타는 시간과 동일하다.
 
   1과 2를 구하기 위해 플로이드-워셜을 이용한다.
   어떤 정점에서 시작했을 때 가장 빠르게 그래프가 불에 타는지 알 수 없기 때문에 브루트포스로 답을 구한다.

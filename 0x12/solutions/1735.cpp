6f2090279606b0169804ad39474c1c59b751f03a
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2023-01-12 18:12:48 +0900

    Update 1735.cpp

--- 0x12/solutions/1735.cpp
+++ 0x12/solutions/1735.cpp
@@ -1,12 +1,17 @@
 // Authored by : scsc3204
-// Co-authored by : -
-// http://boj.kr/8e359fb229604e5f848bf6c9e72afea8
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/ed2a89937cce44aba6b1a18869dd16bc
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b) { return (a ? gcd(b % a, a) : b); }
+int gcd(int a, int b) {
+  if(a == 0) return b;
+  return gcd(b%a, a);
+}
 
-int lcm(int a, int b) { return (a / gcd(a, b) * b); }
+int lcm(int a, int b) {
+  return a / gcd(a, b) * b;
+}
 
 int main() {
   ios::sync_with_stdio(0);
@@ -15,11 +20,11 @@ int main() {
   int a, b, c, d;
   cin >> a >> b >> c >> d; // a/b + c/d
 
-  int l = lcm(b, d);
+  int l = lcm(b, d); // 분모
   a = l / b * a; // al / bl = (al / b) / l
   c = l / d * c; // cl / dl = (cl / d) / l
 
-  int sum = a + c; // {(al / b) + (cl / d)} / l
+  int sum = a + c; // {(al / b) + (cl / d)} / l, 분자
   int g = gcd(l, sum);
 
   cout << sum / g << ' ' << l / g; // 기약분수의 분자 분모 형태로 만듦

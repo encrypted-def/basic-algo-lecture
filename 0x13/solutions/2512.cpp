1ca396d273a9fc5f6cb18224247e65409485bad3
neppiness <0414kjh@naver.com>
2023-01-18 10:59:25 +0900

    update: 0x13 2512.cpp

--- 0x13/solutions/2512.cpp
+++ 0x13/solutions/2512.cpp
@@ -1,11 +1,35 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : scsc3204
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/71f2b814254447fb9efa07dbf0e96768
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int n, budget;
+int a[100'002];
+
+bool solve(int uplim) {
+  int sum = 0;
+  for(int i = 0; i < n; i++)
+    sum += min(a[i], uplim);
+  return budget >= sum;
+}
+
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> n;
+  int st = 1, en = 1;
+  for(int i = 0; i < n; i++) {
+    cin >> a[i];
+    en = max(a[i], en);
+  }
+  cin >> budget;
+
+  while(st < en) {
+    int mid = (st + en) / 2 + 1;
+    if(solve(mid)) st = mid;
+    else en = mid - 1;
+  }
+  cout << st;
 }

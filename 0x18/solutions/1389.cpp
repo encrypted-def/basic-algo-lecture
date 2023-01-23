c2adf0c6f9a5b9ca5dfe02e2def9104c85f307ba
neppiness <0414kjh@naver.com>
2023-01-23 21:58:14 +0900

    update: 0x18 1389.cpp

--- 0x18/solutions/1389.cpp
+++ 0x18/solutions/1389.cpp
@@ -1,11 +1,59 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : scsc3204
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/665d5081e8ef4627b389b0400a340d59
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+const int MX = 100;
+const int INF = 0x7f7f7f7f;
+
+vector<int> adj[MX + 2];
+int dist[MX + 2];
+
+int mn = INF, ans;
+
+int bfs(int st) {
+  queue<int> q;
+  int sum = 0;
+  dist[st] = 0;
+  q.push(st);
+  while(!q.empty()) {
+    int cur = q.front(); q.pop();
+    for(int nxt : adj[cur]) {
+      if(dist[nxt] != -1) continue;
+      dist[nxt] = dist[cur] + 1;
+      sum += dist[nxt];
+      q.push(nxt);
+    }
+  }
+  return sum;
+}
+
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  int n, m; cin >> n >> m;
+  while(m--) {
+    int u, v; cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  for(int i = 1; i <= n; i++) {
+    memset(dist, -1, sizeof(dist));
+    int sum = bfs(i);
+    if(sum >= mn) continue;
+    mn = sum;
+    ans = i;
+  }
+  cout << ans;
 }
+/*
+BFS를 활용한 풀이입니다.
+모든 노드를 기점으로 BFS를 수행하면서 케빈베이컨 수를 구합니다(40-46번째 줄).
+
+가중치가 1인 그래프이므로 BFS를 통해 각 노드까지 최단 거리로 도달할 수 있습니다.
+노드 방문 시 그 노드까지 거리가 결정되므로, 
+구한 거리를 더하면서 sum을 갱신합니다(22번째 줄)
+*/

b9922e108d0806d936965bb2f0188a50ee5aee5f
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2023-01-29 17:10:53 +0900

    Update 1389.cpp

--- 0x18/solutions/1389.cpp
+++ 0x18/solutions/1389.cpp
@@ -1,6 +1,6 @@
 // Authored by : scsc3204
-// Co-authored by : -
-// http://boj.kr/665d5081e8ef4627b389b0400a340d59
+// Co-authored by : BaaaaaaaaaaarkingDog
+// http://boj.kr/a8004409e72f44c7a185802158a79b81
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -41,7 +41,7 @@ int main() {
   }
 
   for(int i = 1; i <= n; i++) {
-    memset(dist, -1, sizeof(dist));
+    fill(dist, dist+n+1, -1);
     int sum = bfs(i);
     if(sum >= mn) continue;
     mn = sum;
@@ -52,7 +52,6 @@ int main() {
 /*
 BFS를 활용한 풀이입니다.
 모든 노드를 기점으로 BFS를 수행하면서 케빈베이컨 수를 구합니다(43-49번째 줄).
-
 가중치가 1인 그래프이므로 BFS를 통해 각 노드까지 최단 거리로 도달할 수 있습니다.
 노드 방문 시 그 노드까지 거리가 결정되므로, 
 구한 거리를 더하면서 sum을 갱신합니다(25번째 줄)

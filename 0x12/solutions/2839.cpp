b74e8acb9b46ec5b9768b138e6b7077d80cf9383
Haneul Kim <95077661+haneulkimdev@users.noreply.github.com>
2023-02-01 22:53:29 +0900

    Update 2839.cpp

--- 0x12/solutions/2839.cpp
+++ 0x12/solutions/2839.cpp
@@ -1,11 +1,23 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : haneulkimdev
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/3649d2bc821d41be958cb51fc82946d3
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  int n;
+  cin >> n;
+  int ans = 0;
+  while (n >= 0) {
+    if (n % 5 == 0) {
+      ans += n / 5;
+      cout << ans;
+      return 0;
+    }
+    n -= 3;
+    ans++;
+  }
+  cout << -1;
 }

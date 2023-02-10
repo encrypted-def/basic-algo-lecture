cc985a2c0781fcfdb5457b1b607e4fd0cb261c24
Haneul Kim <95077661+haneulkimdev@users.noreply.github.com>
2023-02-10 15:47:03 +0900

    Create 1057_1.cpp

---
+++ 0x12/solutions/1057_1.cpp
@@ -0,0 +1,19 @@
+// Authored by : haneulkimdev
+// Co-authored by : -
+// http://boj.kr/ad7d6ee177654e6dbc83931bc7265b7a
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, a, b;
+  cin >> n >> a >> b;
+  int ans = 0;
+  while (a != b) {
+    a = (a + 1) / 2;
+    b = (b + 1) / 2;
+    ans++;
+  }
+  cout << ans;
+}

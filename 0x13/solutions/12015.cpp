9436dcaf1fc19d927d8baa010255296a45cfc927
HyeonJae Park <64001045+HyeonJaePark@users.noreply.github.com>
2023-02-18 04:07:09 +0900

    Update 12015.cpp

--- 0x13/solutions/12015.cpp
+++ 0x13/solutions/12015.cpp
@@ -1,6 +1,6 @@
 // Authored by : HJPark
 // Co-authored by : -
-// http://boj.kr/4e40a01c60d34310b91663881718bd77
+// http://boj.kr/be49a9e53776483988fd1dd46534d1f3
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -10,23 +10,23 @@ int ret[1000005];
 int ans;
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    ret[0] = a[0];
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> a[i];
+  ret[0] = a[0];
 
-    for (int i = 0; i < n; i++) {
-        if (ret[ans] < a[i]) {
-            ret[ans + 1] = a[i];
-            ans++;
-        }
-        else {
-            int idx = lower_bound(ret, ret + ans, a[i]) - ret;
-            ret[idx] = a[i];
-        }
+  for (int i = 0; i < n; i++) {
+    if (ret[ans] < a[i]) {
+      ret[ans + 1] = a[i];
+      ans++;
+    } 
+    else {
+      int idx = lower_bound(ret, ret + ans, a[i]) - ret;
+      ret[idx] = a[i];
     }
+  }
 
-    cout << ans + 1;
+  cout << ans + 1;
 }

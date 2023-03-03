494d4c1fb5112c65d98d64d9bc5956e4e9b09a00
0000000000 <99519474+penguin0000000000@users.noreply.github.com>
2023-03-03 21:03:46 +0900

    Create 9655_1.cpp
    규칙찾기 풀이

---
+++ 0x10/solutions/9655_1.cpp
@@ -0,0 +1,18 @@
+// Authored by : 0000000000
+// Co-authored by : -
+// http://boj.kr/97ae59164b45494fa916d3a4fe2511bc
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int n; cin >> n;
+  
+  cout << (n % 2 ? "SK" : "CY");
+}
+
+/*
+손으로 직접 써 가면서 해 보면 규칙이 나옵니다. 물론 수학적으로도 증명이 가능합니다.
+*/

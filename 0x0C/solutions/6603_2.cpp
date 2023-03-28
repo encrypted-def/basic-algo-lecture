1bced4629dda43725a3dee87b6abd04255b04c64
JunSeok <93777385+CLOUDoort@users.noreply.github.com>
2023-03-28 12:20:02 +0900

    Update 6603_2.cpp

--- 0x0C/solutions/6603_2.cpp
+++ 0x0C/solutions/6603_2.cpp
@@ -1,25 +1,21 @@
+// Authored by : qwee158
+// Co-authored by : -
+// https://www.acmicpc.net/source/58264738
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int k, input[20], arr[10], isUsed[20];
+int k, input[20], arr[10];
 
 void func(int cnt, int cur) {
     if(cnt == 6) {
-        for(int i = 0; i < 6; i++) {
-            cout << arr[i] << ' ';
-        }
+        for(int i = 0; i < 6; i++) cout << arr[i] << ' ';
         cout << '\n';
         return;
     }
-    int temp = 0;
     for(int i = cur; i < k; i++) {
-        if(!isUsed[i] && temp != input[i]) {
-            arr[cnt] = input[i];
-            isUsed[i] = 1;
-            temp = arr[cnt];
-            func(cnt+1, i+1);
-            isUsed[i] = 0;
-        }
+        arr[cnt] = input[i];
+        func(cnt+1, i+1);
     }
 }
 

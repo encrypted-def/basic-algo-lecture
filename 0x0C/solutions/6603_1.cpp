2051f722a6c3a2dd27f2e12d51d8cbb9d3a42005
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2023-04-12 22:00:57 +0900

    Update 6603_1.cpp

--- 0x0C/solutions/6603_1.cpp
+++ 0x0C/solutions/6603_1.cpp
@@ -1,53 +1,31 @@
-// Authored by : yongjunleeme
+// Authored by : qwee158
 // Co-authored by : -
-// http://boj.kr/54ced2c7e27e40da93c95a150d93d56e
+// http://boj.kr/091a628889c24cd79d04dd2abe6e8b5d
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int arr[15];
-int num[15];
-bool isused[15];
+int k, input[20], arr[10];
 
-void func(int k){
-  if(k == 6){
-    bool flag = true;
-    int tmp = -1;
-    for(int i = 0; i < 6; i++){
-      if(tmp > num[arr[i]]) flag = false;
-      tmp = num[arr[i]];
-    }
-
-    if(flag){
-      for(int i = 0; i < 6; i++)
-        cout << num[arr[i]] << " ";
-      cout << "\n";
-    }
+void func(int cnt, int cur) {
+  if (cnt == 6) {
+    for (int i = 0; i < 6; i++) cout << arr[i] << ' ';
+    cout << '\n';
     return;
   }
-  for(int i = 0; i < n; i++){
-    if(!isused[i]){
-      arr[k] = i;
-      isused[i] = 1;
-      func(k+1);
-      isused[i] = 0;
-    }
+  for (int i = cur; i < k; i++) {
+    arr[cnt] = input[i];
+    func(cnt + 1, i + 1);
   }
 }
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  while(1){
-    cin >> n;
-    if(n == 0) break;
-    for(int i = 0; i < n; i++)
-      cin >> num[i];
-    sort(num, num+n);
-    func(0);
-    cout << "\n";
-    fill(num, num+n, 0);
-    fill(arr, arr+n, 0);
+  while (1) {
+    cin >> k;
+    if (k == 0) break;
+    for (int i = 0; i < k; i++) cin >> input[i];
+    func(0, 0);
+    cout << '\n';
   }
 }

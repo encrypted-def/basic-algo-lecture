5208793ac358838063d89fb68401e2c011acf7e7
KB Kim <jimi567@kookmin.ac.kr>
2023-05-02 04:13:48 +0900

    Update 2283.cpp

--- 0x14/solutions/2283.cpp
+++ 0x14/solutions/2283.cpp
@@ -7,25 +7,24 @@ using ll = long long;
 int n, k;
 int arr[1000005];
 int main(void) {
-	ios::sync_with_stdio(0); 
+  ios::sync_with_stdio(0);
   cin.tie(0); 
-  
-	cin >> n >> k;
-	for (int i = 0;i < n;i++) {
-		int st, en;
-		cin >> st >> en;
-		for (;st+1 <= en;st++) arr[st]++;
-	}
-	int st = 0; int en = 0;
-	int sum = 0;
-	while (st <= en && en<=1000000 ) {
-	  if (sum == k) {
-			cout << st << " " << en;
-			return 0;
-		}
-		else if (sum < k) sum += arr[en++];
-		else sum -= arr[st++];
-	}
-	cout << 0 << " " << 0;
-    return 0;
+  cin >> n >> k;
+  for (int i = 0;i < n;i++) {
+    int st, en;
+    cin >> st >> en;
+    for (;st+1 <= en;st++) arr[st]++;
+  }
+  int st = 0; int en = 0;
+  int sum = 0;
+  while (st <= en && en<=1000000 ) {
+    if (sum == k) {
+      cout << st << " " << en;
+      return 0;
+    }
+    else if (sum < k) sum += arr[en++];
+    else sum -= arr[st++];
+  }
+  cout << 0 << " " << 0;
+  return 0;
 }

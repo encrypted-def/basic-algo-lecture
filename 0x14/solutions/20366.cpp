6d1c1cf385168a84a5481601c082246ab88c9b78
Seungyun Oh <syoh0708@kaist.ac.kr>
2023-05-09 12:41:29 +0900

    Create 20366.cpp

--- 0x14/solutions/20366.cpp
+++ 0x14/solutions/20366.cpp
@@ -1,11 +1,36 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : syoh0708
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/3589e6590845469191b7873b71550403
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main(void){
+const int MAX = 1'000'000'000;
+int n, ans = MAX;
+int a[605];
+
+int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) cin >> a[i];
+
+  sort(a, a + n);
+
+  for (int i1 = 0; i1 < n; i1++) {
+    for (int i2 = i1 + 3; i2 < n; i2++) {
+      int j1 = i1 + 1, j2 = i2 - 1;
+
+      while (j1 < j2) {
+        ans = min(ans, abs(a[i1] + a[i2] - a[j1] - a[j2]));
+
+        if (a[i1] + a[i2] <= a[j1] + a[j2]) j2--;
+        else j1++;
+      }
+    }
+  }
+
+  cout << ans;
 }

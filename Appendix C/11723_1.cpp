1e91032ec3b7d66d667deda1fa7a4919e0f4ad72
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2024-03-17 09:27:14 +0900

    Update 11723_1.cpp

--- Appendix C/11723_1.cpp
+++ Appendix C/11723_1.cpp
@@ -1,8 +1,8 @@
-// http://boj.kr/1febea8e97bb4493925075f36d6efb8d
+// http://boj.kr/7c4a1a954b7f4aa19635cf6dcfc5a5c6
 #include <bits/stdc++.h>
 using namespace std;
 
-int chk[20];
+int state[20];
 
 int main(){
   ios::sync_with_stdio(0);
@@ -16,25 +16,25 @@ int main(){
     cin >> com;
     if(com == "add"){
       cin >> x;
-      chk[x-1] = 1;      
+      state[x-1] = 1;      
     }
     else if(com == "remove"){
       cin >> x;
-      chk[x-1] = 0;
+      state[x-1] = 0;
     }
     else if(com == "check"){
       cin >> x;
-      cout << chk[x-1] << '\n';
+      cout << state[x-1] << '\n';
     }
     else if(com == "toggle"){
       cin >> x;
-      chk[x-1] = 1 ^ chk[x-1];
+      state[x-1] = 1 ^ state[x-1];
     }
     else if(com == "all"){
-      fill(chk, chk+20, 1);
+      fill(state, state+20, 1);
     }
     else{ // empty
-      fill(chk, chk+20, 0);
+      fill(state, state+20, 0);
     }
   }
 }

5c7ef633bdc591886f162e91d04459442ca12ec9
paulcjy <86853786+paulcjy@users.noreply.github.com>
2024-03-27 21:49:36 +0900

    Create 2448_1.cpp

--- 0x0B/solutions/2448_1.cpp
+++ 0x0B/solutions/2448_1.cpp
@@ -1,10 +1,13 @@
+// Authored by : paulcjy
+// Co-authored by : -
+// http://boj.kr/b510439ad07b43d2b6a689ff104c3783
 #include <bits/stdc++.h>
 using namespace std;
 
-void printStar(int n, int i, int j) {
+void print_star(int n, int i, int j) {
   if (j <= 2*i)
     if (n == 3 && !(i == 1 && j == 1)) cout << '*';
-    else printStar(n/2, i%(n/2), j%n);
+    else print_star(n/2, i%(n/2), j%n);
   else cout << ' ';
 }
 
@@ -16,7 +19,7 @@ int main() {
   for (int i = 0; i < n; i++) {
     cout << string(n-i-1, ' ');
     for (int j = 0; j <= n+i; j++)
-      printStar(n, i, j);
+      print_star(n, i, j);
     cout << '\n';
   }
 }

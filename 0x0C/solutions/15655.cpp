ba3b2fbfb945aecdc30e36e034169abcb58675c8
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2024-11-07 23:44:55 +0900

    Update 15655.cpp

--- 0x0C/solutions/15655.cpp
+++ 0x0C/solutions/15655.cpp
@@ -1,6 +1,6 @@
 // Authored by : BaaaaaaaaaaarkingDog
-// Co-authored by : -
-// http://boj.kr/19fa292ed77f4bd48cbca269aafd4686
+// Co-authored by : Kim-AYoung
+// http://boj.kr/2cd18796fe364e7c95cd1764d73e15e8
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -19,12 +19,8 @@ void func(int k){ // 현재 k개까지 수를 택했음.
   int st = 0; // 시작지점, k = 0일 때에는 st = 0
   if(k != 0) st = arr[k-1] + 1; // k != 0일 경우 st = arr[k-1]+1
   for(int i = st; i < n; i++){
-    if(!isused[i]){ // 아직 i가 사용되지 않았으면
-      arr[k] = i; // k번째 인덱스를 i로 정함
-      isused[i] = 1; // i를 사용되었다고 표시
-      func(k+1); // 다음 인덱스를 정하러 한 단계 더 들어감
-      isused[i] = 0; // k번째 인덱스를 i로 정한 모든 경우에 대해 다 확인했으니 i를 이제 사용되지않았다고 명시함.
-    }
+    arr[k] = i; // k번째 인덱스를 i로 정함
+    func(k+1); // 다음 인덱스를 정하러 한 단계 더 들어감
   }
 }
 

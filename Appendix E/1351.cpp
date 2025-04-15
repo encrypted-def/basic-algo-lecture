2303e9e471f83a0cf31a31834f4b9e22fc7b0861
encrypted-def <20028331+encrypted-def@users.noreply.github.com>
2025-04-15 20:54:45 +0900

    Create 1351.cpp

---
+++ Appendix E/1351.cpp
@@ -0,0 +1,23 @@
+// http://boj.kr/5a8c26a75fd7433da8b814fc477b0317
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+ll n, p, q;
+map<ll, ll> a;
+
+ll go(ll x){
+  if(a[x] != 0)
+    return a[x];
+  if(x == 0)
+    return a[x] = 1;
+  return a[x] = go(x/p) + go(x/q);
+}
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n >> p >> q;
+  cout << go(n);
+}

df0bc620412285613cbb5005f4d8784a8e3bb849
qwer1234qwer1234qwer <oiooii1115@gmail.com>
2025-06-01 00:50:19 +0900

    Create 2753_1.cpp

---
+++ 0x02/solutions/2753_1.cpp
@@ -0,0 +1,20 @@
+﻿// Authored by : -
+// Co-authored by : -
+//http://boj.kr/4b1e161ffc9b4281ab7dad4d1b6cbbd9
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int year;
+  cin >> year;
+
+  cout << chrono::year(year).is_leap();
+}
+
+/*
+https://en.cppreference.com/w/cpp/chrono/year/is_leap
+*/

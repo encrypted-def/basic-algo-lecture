170dd6a1e6af89bc1d3fe8ddcd84640c5b9ce20d
정유환 <jeong-yuhwan@jeong-yuhwan-ui-noteubug.local>
2025-09-29 10:49:05 +0900

    25515번 풀이 추가

--- Appendix E/solutions/25515.cpp
+++ Appendix E/solutions/25515.cpp
@@ -1,11 +1,38 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : uhwan0723
 // Co-authored by : -
-// http://boj.kr/****************
+// http://boj.kr/190a0fda546b4e47971e626d4a329620
 #include <bits/stdc++.h>
 using namespace std;
+
+const int mn = 1e5 + 5;
+int n, val[mn];
+vector<int> adj[mn];
+
+/*
+dfs(cur) = 정점 cur을 방문할 때 정수 합의 최댓값
+         = cur의 모든 자식 ch에 대해 max(0, dfs(cur, ch)) 값들의 합 + 정점 cur에 적힌 정수
+*/
+long long dfs(int par, int cur){
+  long long res = val[cur];
+  for(int nxt : adj[cur]){
+    if(nxt == par) continue;
+    res += max(1LL*0, dfs(cur, nxt));
+  }
+  return res;
+}
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
+  cin >> n;
+  for(int i = 0; i < n-1; ++i){
+    int u, v;
+    cin >> u >> v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  for(int i = 0; i < n; ++i)
+    cin >> val[i];
+
+  cout << dfs(-1, 0);
 }
